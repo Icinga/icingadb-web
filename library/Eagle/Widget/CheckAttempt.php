2b7d3d2cd4681575d034005e3821044beebe122a
Eric Lippmann <eric.lippmann@icinga.com>
2019-09-09 14:45:45 +0200

    Add CheckAttempt widget

---
+++ library/Eagle/Widget/CheckAttempt.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Icinga\Module\Eagle\Widget;
+
+use ipl\Html\BaseHtmlElement;
+use ipl\Html\FormattedString;
+
+/**
+ * Visually represents the check attempts taken out of max check attempts.
+ */
+class CheckAttempt extends BaseHtmlElement
+{
+    protected $tag = 'div';
+
+    protected $defaultAttributes = ['class' => 'check-attempt'];
+
+    /** @var int Current attempt */
+    protected $attempt;
+
+    /** @var int Max check attempts */
+    protected $maxAttempts;
+
+    /**
+     * Create a new check attempt widget
+     *
+     * @param int $attempt     Current check attempt
+     * @param int $maxAttempts Max check attempts
+     */
+    public function __construct($attempt, $maxAttempts)
+    {
+        $this->attempt = $attempt;
+        $this->maxAttempts = $maxAttempts;
+    }
+
+    protected function assemble()
+    {
+        if ($this->maxAttempts > 5) {
+            $this->add(new FormattedString('%d/%d', $this->attempt, $this->maxAttempts));
+        } else {
+            for ($i = 0; $i < $this->attempt; ++$i) {
+                $this->add(new AttemptBall(true));
+            }
+            for ($i = $this->attempt; $i < $this->maxAttempts; ++$i) {
+                $this->add(new AttemptBall());
+            }
+        }
+    }
+}
