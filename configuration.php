1424ca93ab3d1809a11ac94cd06aff90724fd71f
Eric Lippmann <eric.lippmann@icinga.com>
2019-09-09 14:49:46 +0200

    CSS: Add rules for .ball and ball-size-*

---
+++ configuration.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Icinga\Module\Eagle
+{
+    /** @var \Icinga\Application\Modules\Module $this */
+    $section = $this->menuSection(N_('Icinga DB'), [
+        'icon'     => 'database',
+        'priority' => 30
+    ]);
+
+    $section->add(N_('Hosts'), [
+        'url' => 'eagle/hosts',
+        'priority' => 10
+    ]);
+}
