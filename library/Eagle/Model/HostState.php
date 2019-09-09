cc3a813f82c10b8c845e5069c7630be0adedbf34
Eric Lippmann <eric.lippmann@icinga.com>
2019-09-09 13:17:28 +0200

    Add HostState model

---
+++ library/Eagle/Model/HostState.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Icinga\Module\Eagle\Model;
+
+use Icinga\Module\Eagle\Common\HostStates;
+use ipl\Orm\Relations;
+
+/**
+ * Host state model.
+ */
+class HostState extends State
+{
+    public function getTableName()
+    {
+        return 'host_state';
+    }
+
+    public function getKeyName()
+    {
+        return 'host_id';
+    }
+
+    public function createRelations(Relations $relations)
+    {
+        $relations->belongsTo('host', Host::class);
+    }
+
+    /**
+     * Get the host state as the textual representation
+     *
+     * @return string
+     */
+    public function getStateText()
+    {
+        return HostStates::text($this->properties['soft_state']);
+    }
+
+    /**
+     * Get the host state as the translated textual representation
+     *
+     * @return string
+     */
+    public function getStateTextTranslated()
+    {
+        return HostStates::text($this->properties['soft_state']);
+    }
+}
