2aac205189181e86403333a56e691742a2224196
Eric Lippmann <eric.lippmann@icinga.com>
2019-10-08 23:58:10 +0200

    Apply view mode in HostList

--- library/Eagle/Widget/HostList.php
+++ library/Eagle/Widget/HostList.php
@@ -2,17 +2,29 @@
 
 namespace Icinga\Module\Eagle\Widget;
 
+use Icinga\Module\Eagle\Common\ViewMode;
+
 /**
  * Host list
  */
 class HostList extends StateList
 {
+    use ViewMode;
+
     protected $tag = 'ul';
 
     protected $defaultAttributes = ['class' => 'object-list', 'data-base-target' => '_next'];
 
     protected function getItemClass()
     {
+        $viewMode = $this->getViewMode();
+
+        $this->addAttributes(['class' => $viewMode]);
+
+        if ($viewMode === 'compact') {
+            return HostListItemCompact::class;
+        }
+
         return HostListItem::class;
     }
 }
