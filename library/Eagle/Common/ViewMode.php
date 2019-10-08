f798653e65c7f3c6a089c497689df9a95aad8c5b
Eric Lippmann <eric.lippmann@icinga.com>
2019-10-08 23:57:34 +0200

    Introduce ViewMode trait

---
+++ library/Eagle/Common/ViewMode.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Icinga\Module\Eagle\Common;
+
+trait ViewMode
+{
+    /** @var string */
+    protected $viewMode;
+
+    /**
+     * Get the view mode
+     *
+     * @return string
+     */
+    public function getViewMode()
+    {
+        return $this->viewMode;
+    }
+
+    /**
+     * Set the view mode
+     *
+     * @param string $viewMode
+     *
+     * @return $this
+     */
+    public function setViewMode($viewMode)
+    {
+        $this->viewMode = $viewMode;
+
+        return $this;
+    }
+}
