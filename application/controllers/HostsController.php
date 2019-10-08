a6d65bb37580025b4f5b61316aa3df162c24ee04
Eric Lippmann <eric.lippmann@icinga.com>
2019-10-09 00:09:18 +0200

    Add limit control to host list

--- application/controllers/HostsController.php
+++ application/controllers/HostsController.php
@@ -2,6 +2,7 @@
 
 namespace Icinga\Module\Eagle\Controllers;
 
+use GuzzleHttp\Psr7\ServerRequest;
 use Icinga\Module\Eagle\Model\Host;
 use Icinga\Module\Eagle\Web\Controller;
 use Icinga\Module\Eagle\Widget\HostList;
@@ -15,15 +16,19 @@ class HostsController extends Controller
         $db = $this->getDb();
 
         $hosts = Host::on($db)->with('state');
-        $hosts->limit(25);
+        // @TODO(el): Looks a bit odd since createLimitControl() also shifts the limit
+        $hosts->limit($this->params->shift('limit'));
 
         $viewModeSwitcher = $this->createViewModeSwitcher();
+        $limitControl = $this->createLimitControl();
+        $limitControl->handleRequest(ServerRequest::fromGlobals());
 
         $hostList = (new HostList($hosts))
             ->setRedis($this->getRedis())
             ->setViewMode($viewModeSwitcher->getViewMode());
 
         $this->addControl($viewModeSwitcher);
+        $this->addControl($limitControl);
 
         $this->addContent($hostList);
     }
