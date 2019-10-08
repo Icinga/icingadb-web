8fbc50ddc2064d9665a00c8ecb1d7e26b5e80d22
Florian Strohmaier <florian.strohmaier@icinga.com>
2019-10-08 17:47:13 +0200

    Introduce class Icons

---
+++ library/Eagle/Common/Icons.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Icinga\Module\Eagle\Common;
+
+class Icons
+{
+    const HOST_DOWN = 'sitemap';
+
+    const IN_DOWNTIME = 'plug';
+
+    const IS_ACKNOWLEDGED = 'ok';
+
+    const IS_FLAPPING = 'flash';
+
+    const WARNING = 'warning-empty';
+}
