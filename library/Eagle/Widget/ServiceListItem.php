06ba518963f3bf653ef8ef4523b5200a6fdbbbf1
Eric Lippmann <eric.lippmann@icinga.com>
2019-10-09 17:23:05 +0200

    Introduce ServiceListItem

---
+++ library/Eagle/Widget/ServiceListItem.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Icinga\Module\Eagle\Widget;
+
+use Icinga\Module\Eagle\Common\ListItemCommonLayout;
+
+class ServiceListItem extends BaseServiceListItem
+{
+    use ListItemCommonLayout;
+}
