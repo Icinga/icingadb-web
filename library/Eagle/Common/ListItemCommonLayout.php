06897601f2b28def242dab9bef84603ef7595608
Eric Lippmann <eric.lippmann@icinga.com>
2019-10-09 17:20:24 +0200

    Introduce ListItemCommonLayout trait

---
+++ library/Eagle/Common/ListItemCommonLayout.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Icinga\Module\Eagle\Common;
+
+use ipl\Html\BaseHtmlElement;
+
+trait ListItemCommonLayout
+{
+    protected function assembleHeader(BaseHtmlElement $header)
+    {
+        $header->add($this->createTitle());
+        $header->add($this->createTimestamp());
+    }
+
+    protected function assembleMain(BaseHtmlElement $main)
+    {
+        $main->add($this->createHeader());
+        $main->add($this->createCaption());
+    }
+}
