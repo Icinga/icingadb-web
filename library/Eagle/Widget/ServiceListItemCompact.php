b30082da3ee0ef1f2710297e8c5ca2eea10c132c
Eric Lippmann <eric.lippmann@icinga.com>
2019-10-09 17:23:14 +0200

    Introduce ServiceListItemCompact

---
+++ library/Eagle/Widget/ServiceListItemCompact.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Icinga\Module\Eagle\Widget;
+
+use Icinga\Module\Eagle\Common\ListItemCompactLayout;
+
+class ServiceListItemCompact extends BaseServiceListItem
+{
+    use ListItemCompactLayout;
+}
