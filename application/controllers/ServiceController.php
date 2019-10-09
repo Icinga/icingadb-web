3c94acc650a7e7a450b0a681f297b7eb020c6cb9
Johannes Meyer <johannes.meyer@icinga.com>
2019-10-09 17:26:52 +0200

    Introduce ServiceController

---
+++ application/controllers/ServiceController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Icinga\Module\Eagle\Controllers;
+
+use Icinga\Exception\NotFoundError;
+use Icinga\Module\Eagle\Model\Service;
+use Icinga\Module\Eagle\Redis\VolatileState;
+use Icinga\Module\Eagle\Web\Controller;
+use Icinga\Module\Eagle\Widget\ServiceListItem;
+
+class ServiceController extends Controller
+{
+    public function indexAction()
+    {
+        $name = $this->params->shiftRequired('name');
+        $hostName = $this->params->shiftRequired('host_name');
+
+        $query = Service::on($this->getDb())->with([
+            'state',
+            'host'
+        ]);
+        $query->getSelectBase()
+            ->where(['service.name = ?' => $name])
+            ->where(['host.name = ?' => $hostName]);
+
+        /** @var Service $service */
+        $service = $query->first();
+        if ($service === null) {
+            throw new NotFoundError($this->translate('Service not found'));
+        }
+
+        $volatileState = new VolatileState($this->getRedis());
+        $volatileState->add($service);
+        $volatileState->fetch();
+
+        $this->addContent((new ServiceListItem($service))->setTag('div'));
+    }
+}
