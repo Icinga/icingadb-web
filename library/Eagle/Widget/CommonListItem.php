6eab2c4ad3ff1f4bbdf9ad34596ed1a82a0a2ef8
Eric Lippmann <eric.lippmann@icinga.com>
2019-10-09 17:21:36 +0200

    Introduce CommonListItem

---
+++ library/Eagle/Widget/CommonListItem.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Icinga\Module\Eagle\Widget;
+
+use Icinga\Module\Eagle\Common\ListItemCommonLayout;
+
+abstract class CommonListItem extends BaseListItem
+{
+    use ListItemCommonLayout;
+}
