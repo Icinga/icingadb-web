aff95a3537095b8141130adb2ff4dca7c649f57f
Eric Lippmann <eric.lippmann@icinga.com>
2019-10-09 17:24:40 +0200

    Introduce ServiceStates

---
+++ library/Eagle/Common/ServiceStates.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Icinga\Module\Eagle\Common;
+
+/**
+ * Collection of possible service states.
+ */
+class ServiceStates
+{
+    const OK = 0;
+
+    const WARNING = 1;
+
+    const CRITICAL = 2;
+
+    const UNKNOWN = 3;
+
+    const PENDING = 99;
+
+    /**
+     * Get the textual representation of the passed service state
+     *
+     * @param int $state
+     *
+     * @return string
+     *
+     * @throws \InvalidArgumentException If the given service state is invalid, i.e. not known
+     */
+    public static function text($state)
+    {
+        switch ((int) $state) {
+            case self::OK:
+                $text = 'ok';
+                break;
+            case self::WARNING:
+                $text = 'warning';
+                break;
+            case self::CRITICAL:
+                $text = 'critical';
+                break;
+            case self::UNKNOWN:
+                $text = 'unkown';
+                break;
+            case self::PENDING:
+                $text = 'pending';
+                break;
+            default:
+                throw new \InvalidArgumentException(sprintf('Invalid service state %d', $state));
+        }
+
+        return $text;
+    }
+
+    /**
+     * Get the translated textual representation of the passed service state
+     *
+     * @param int $state
+     *
+     * @return string
+     *
+     * @throws \InvalidArgumentException If the given service state is invalid, i.e. not known
+     */
+    public static function translated($state)
+    {
+        switch ((int) $state) {
+            case self::OK:
+                $text = mt('eagle', 'ok');
+                break;
+            case self::WARNING:
+                $text = mt('eagle', 'warning');
+                break;
+            case self::CRITICAL:
+                $text = mt('eagle', 'critical');
+                break;
+            case self::UNKNOWN:
+                $text = mt('eagle', 'unknown');
+                break;
+            case self::PENDING:
+                $text = mt('eagle', 'pending');
+                break;
+            default:
+                throw new \InvalidArgumentException(sprintf('Invalid service state %d', $state));
+        }
+
+        return $text;
+    }
+}
