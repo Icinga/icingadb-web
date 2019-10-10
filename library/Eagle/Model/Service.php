4d78d2fa2888bff77ebc3eee7b7cb35db29d0df1
Eric Lippmann <eric.lippmann@icinga.com>
2019-10-10 15:14:08 +0200

    Set table alias

--- library/Eagle/Model/Service.php
+++ library/Eagle/Model/Service.php
@@ -82,7 +82,7 @@ class Service extends Model
         $relations->belongsToMany('servicegroup', Servicegroup::class)
             ->setThrough(ServicegroupMember::class);
 
-        $relations->hasOne('state', ServiceState::class);
+        $relations->hasOne('state', ServiceState::class)->setTableAlias('service_state');
         $relations->hasMany('comment', ServiceComment::class);
         $relations->hasMany('downtime', ServiceDowntime::class);
         $relations->hasMany('notification', Notification::class);
