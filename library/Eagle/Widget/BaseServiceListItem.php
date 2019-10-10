cfe370e1432850304244f1a9c5decd4ee6a5e16b
Florian Strohmaier <florian.strohmaier@icinga.com>
2019-10-10 15:14:32 +0200

    Add host link to service list items

--- library/Eagle/Widget/BaseServiceListItem.php
+++ library/Eagle/Widget/BaseServiceListItem.php
@@ -4,6 +4,7 @@ namespace Icinga\Module\Eagle\Widget;
 
 use ipl\Html\Html;
 use ipl\Web\Url;
+use ipl\Web\Widget\StateBall;
 
 /**
  * Service item of a service list. Represents one database row.
@@ -12,12 +13,31 @@ abstract class BaseServiceListItem extends StateListItem
 {
     protected function createSubject()
     {
-        return Html::tag('a', [
-            'href' => Url::fromPath('eagle/service', [
-                'name'      => $this->item->name,
-                'host_name' => $this->item->host->name
-            ]),
-            'class' => 'subject'
-        ], $this->item->display_name);
+        return [
+            Html::tag(
+                'a',
+                [
+                    'href'  => Url::fromPath('eagle/service', [
+                        'name'      => $this->item->name,
+                        'host_name' => $this->item->host->name
+                    ]),
+                    'class' => 'subject'
+                ],
+                $this->item->display_name
+            ),
+            ' on ',
+            Html::tag(
+                'a',
+                [
+                    'href'  => Url::fromPath('eagle/host', ['name' => $this->item->host->name]),
+                    'class' => 'subject'
+                ],
+                [
+                    new StateBall($this->item->host->state->getStateText(), StateBall::SIZE_MEDIUM),
+                    ' ',
+                    $this->item->host->display_name
+                ]
+            )
+        ];
     }
 }
