623fd8b1ffee4f046c2a5fc966439a776e0fec12
Johannes Meyer <johannes.meyer@icinga.com>
2019-10-10 13:41:12 +0200

    HostController: Use trait `CommandActions`

--- application/controllers/HostController.php
+++ application/controllers/HostController.php
@@ -3,6 +3,7 @@
 namespace Icinga\Module\Eagle\Controllers;
 
 use Icinga\Exception\NotFoundError;
+use Icinga\Module\Eagle\Common\CommandActions;
 use Icinga\Module\Eagle\Model\Host;
 use Icinga\Module\Eagle\Redis\VolatileState;
 use Icinga\Module\Eagle\Web\Controller;
@@ -10,7 +11,12 @@ use Icinga\Module\Eagle\Widget\HostListItem;
 
 class HostController extends Controller
 {
-    public function indexAction()
+    use CommandActions;
+
+    /** @var Host The host object */
+    protected $host;
+
+    public function init()
     {
         $name = $this->params->shiftRequired('name');
 
@@ -28,6 +34,16 @@ class HostController extends Controller
         $volatileState->add($host);
         $volatileState->fetch();
 
-        $this->addContent((new HostListItem($host))->setTag('div'));
+        $this->host = $host;
+    }
+
+    protected function fetchCommandTargets()
+    {
+        return [$this->host];
+    }
+
+    public function indexAction()
+    {
+        $this->addContent((new HostListItem($this->host))->setTag('div'));
     }
 }
