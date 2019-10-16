a9f076adf9a67d941ab7f3e7190b24c105d0695f
Marius Hein <marius.hein@icinga.com>
2019-10-16 16:34:39 +0200

    Introduce class DateFormatter

---
+++ library/Eagle/Date/DateFormatter.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Icinga\Module\Eagle\Date;
+
+class DateFormatter
+{
+    /**
+     * Format a duration
+     *
+     * @param int|float $seconds Duration in seconds
+     * @param bool      $short
+     *
+     * @return string
+     */
+    public static function formatDuration($seconds, $short = false)
+    {
+        if ($short && $seconds > 3600) {
+            $seconds = $seconds + (3600 - ($seconds % 3600));
+        }
+
+        $minutes = floor((float) $seconds / 60);
+        if ($minutes < 60) {
+            $formatted = sprintf('%dm %ds', $minutes, $seconds % 60);
+        } else {
+            $hours = floor($minutes / 60);
+            if ($hours < 24) {
+                $formatted = sprintf('%dh %dm', $hours, $minutes % 60);
+            } else {
+                $formatted = sprintf('%dd %dh', floor($hours / 24), $hours % 24);
+            }
+        }
+
+        if ($short) {
+            return explode(' ', $formatted, 2)[0];
+        }
+
+        return $formatted;
+    }
+}
