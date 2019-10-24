1154452023c8018bf6e1bdd34af7856bf9790b4b
Florian Strohmaier <florian.strohmaier@icinga.com>
2019-10-24 14:04:20 +0200

    Add UserGroupsController

---
+++ application/controllers/UsergroupsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Icinga\Module\Eagle\Controllers;
+
+use Icinga\Module\Eagle\Model\Usergroup;
+use Icinga\Module\Eagle\Web\Controller;
+use Icinga\Module\Eagle\Widget\ItemList\UsergroupList;
+
+class UsergroupsController extends Controller
+{
+    public function indexAction()
+    {
+        $this->setTitle($this->translate('User Groups'));
+
+        $db = $this->getDb();
+
+        $usergroups = Usergroup::on($db)->with('user');
+
+        $limitControl = $this->createLimitControl();
+        $paginationControl = $this->createPaginationControl($usergroups);
+        $filterControl = $this->createFilterControl($usergroups);
+
+        $this->filter($usergroups);
+
+        yield $this->export($usergroups);
+
+        $usergroupList = new UsergroupList($usergroups);
+
+        $this->addControl($paginationControl);
+        $this->addControl($limitControl);
+        $this->addControl($filterControl);
+
+        $this->addContent($usergroupList);
+    }
+}
