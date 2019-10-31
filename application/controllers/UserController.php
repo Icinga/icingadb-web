4eb171e9feef5dae4943e44cbd27ba3dd4db0cce
Florian Strohmaier <florian.strohmaier@icinga.com>
2019-10-31 17:40:31 +0100

    Introduce UserController

---
+++ application/controllers/UserController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Icinga\Module\Eagle\Controllers;
+
+use Icinga\Exception\NotFoundError;
+use Icinga\Module\Eagle\Model\User;
+use Icinga\Module\Eagle\Web\Controller;
+use Icinga\Module\Eagle\Widget\ItemList\UserList;
+use ipl\Html\Html;
+
+class UserController extends Controller
+{
+    /** @var User The user object */
+    protected $user;
+
+    public function init()
+    {
+        $this->setTitle($this->translate('User'));
+
+        $name = $this->params->shiftRequired('name');
+
+        $query = User::on($this->getDb());
+        $query->getSelectBase()
+            ->where(['name = ?' => $name]);
+
+        $user = $query->first();
+        if ($user === null) {
+            throw new NotFoundError($this->translate('User not found'));
+        }
+
+        $this->user = $user;
+    }
+
+    public function indexAction()
+    {
+        $this->addControl(new UserList([$this->user]));
+
+        $this->addContent(Html::tag('h2', 'Details'));
+        $this->addContent(Html::tag('ul', ['class' => 'key-value-list'], [
+            Html::tag('li', [
+                Html::tag('span', ['class' => 'label'], 'E-Mail'),
+                Html::tag(
+                    'span',
+                    ['class' => 'value'],
+                    $this->user->email ?: Html::tag('span', ['class' => 'text-muted'], 'Unset')
+                )
+            ]),
+            Html::tag('li', [
+                Html::tag('span', ['class' => 'label'], 'Pager'),
+                Html::tag(
+                    'span',
+                    ['class' => 'value'],
+                    $this->user->pager ?: Html::tag('span', ['class' => 'text-muted'], 'Unset')
+                )
+            ])
+        ]));
+    }
+}
