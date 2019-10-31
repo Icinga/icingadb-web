17a56c6d3b9eeb8c33dfe0800209a543941392d2
Eric Lippmann <eric.lippmann@icinga.com>
2019-10-31 14:24:57 +0100

    Introduce widget StateBadge

---
+++ library/Eagle/Widget/StateBadge.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Icinga\Module\Eagle\Widget;
+
+use ipl\Html\BaseHtmlElement;
+
+class StateBadge extends BaseHtmlElement
+{
+    protected $defaultAttributes = ['class' => 'state-badge'];
+
+    /** @var mixed Badge content */
+    protected $content;
+
+    /** @var bool Whether the state is handled */
+    protected $isHandled;
+
+    /** @var string Textual representation of a state */
+    protected $state;
+
+    /**
+     * Create a new state badge
+     *
+     * @param mixed  $content   Content of the badge
+     * @param string $state     Textual representation of a state
+     * @param bool   $isHandled True if state is handled
+     */
+    public function __construct($content, $state, $isHandled = false)
+    {
+        $this->content = $content;
+        $this->isHandled = $isHandled;
+        $this->state = $state;
+    }
+
+    protected function assemble()
+    {
+        $this->setTag('span');
+
+        $class = "state-{$this->state}";
+        if ($this->isHandled) {
+            $class .= ' handled';
+        }
+
+        $this->addAttributes(['class' => $class]);
+
+        $this->add($this->content);
+    }
+}
