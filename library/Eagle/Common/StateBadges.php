6a96702d753d7dac7bf26ce7724149245e5f8474
Eric Lippmann <eric.lippmann@icinga.com>
2019-10-31 14:24:31 +0100

    Introduce class StateBadges

---
+++ library/Eagle/Common/StateBadges.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Icinga\Module\Eagle\Common;
+
+use Icinga\Module\Eagle\Widget\StateBadge;
+use ipl\Html\BaseHtmlElement;
+use ipl\Html\Html;
+
+abstract class StateBadges extends BaseHtmlElement
+{
+    /** @var object $item */
+    protected $item;
+
+    /** @var string Prefix */
+    protected $prefix;
+
+    protected $tag = 'ul';
+
+    protected $defaultAttributes = ['class' => 'state-badges'];
+
+    /**
+     * Create a new widget for state badges
+     *
+     * @param object $item
+     */
+    public function __construct($item)
+    {
+        $this->item = $item;
+        $this->prefix = $this->getPrefix();
+    }
+
+    abstract protected function getPrefix();
+
+    protected function createBadge($state)
+    {
+        $key = $this->prefix . "_{$state}";
+
+        if (isset($this->item->$key) && $this->item->$key) {
+            return Html::tag('li', new StateBadge($this->item->$key, $state));
+        }
+
+        return null;
+    }
+
+    protected function createGroup($state)
+    {
+        $content = [];
+        $handledKey = $this->prefix . "_{$state}_handled";
+        $unhandledKey = $this->prefix . "_{$state}_unhandled";
+
+        if (isset($this->item->$unhandledKey) && $this->item->$unhandledKey) {
+            $content[] = Html::tag('li', new StateBadge($this->item->$unhandledKey, $state));
+        }
+
+        if (isset($this->item->$handledKey) && $this->item->$handledKey) {
+            $content[] = Html::tag('li', new StateBadge($this->item->$handledKey, $state, true));
+        }
+
+        if (empty($content)) {
+            return null;
+        }
+
+        return Html::tag('li', Html::tag('ul', $content));
+    }
+}
