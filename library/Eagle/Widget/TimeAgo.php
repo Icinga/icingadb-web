1eb52f1edecab19ec1c2b764e91b4fee258af7ec
Eric Lippmann <eric.lippmann@icinga.com>
2019-11-03 13:36:14 +0100

    Introduce widget TimeAgo

---
+++ library/Eagle/Widget/TimeAgo.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Icinga\Module\Eagle\Widget;
+
+use Icinga\Date\DateFormatter;
+use ipl\Html\BaseHtmlElement;
+
+/**
+ * @TODO(el): Move to ipl-web
+ */
+class TimeAgo extends BaseHtmlElement
+{
+    /** @var int */
+    protected $ago;
+
+    protected $tag = 'time';
+
+    protected $defaultAttributes = ['class' => 'time-ago'];
+
+    public function __construct($ago)
+    {
+        $this->ago = (int) $ago;
+    }
+
+    protected function assemble()
+    {
+        $dateTime = DateFormatter::formatDateTime($this->ago);
+
+        $this->addAttributes([
+            'datetime' => $dateTime,
+            'title'    => $dateTime
+        ]);
+
+        $this->add(DateFormatter::timeAgo($this->ago));
+    }
+}
