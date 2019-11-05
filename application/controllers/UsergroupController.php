f33da8cd7d959605a0b78d266d3054a6b9ef9004
Eric Lippmann <eric.lippmann@icinga.com>
2019-11-05 09:17:11 +0100

    Introduce UsergroupController

---
+++ application/controllers/UsergroupController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Icinga\Module\Icingadb\Controllers;
+
+use Icinga\Exception\NotFoundError;
+use Icinga\Module\Icingadb\Model\Usergroup;
+use Icinga\Module\Icingadb\Web\Controller;
+use Icinga\Module\Icingadb\Widget\ItemList\UsergroupList;
+use Icinga\Module\Icingadb\Widget\ItemList\UserList;
+use ipl\Html\Html;
+
+class UsergroupController extends Controller
+{
+    /** @var Usergroup The usergroup object */
+    protected $usergroup;
+
+    public function init()
+    {
+        $this->setTitle($this->translate('User Group'));
+
+        $name = $this->params->shiftRequired('name');
+
+        $query = Usergroup::on($this->getDb());
+        $query->getSelectBase()
+            ->where(['name = ?' => $name]);
+
+        $usergroup = $query->first();
+        if ($usergroup === null) {
+            throw new NotFoundError($this->translate('User group not found'));
+        }
+
+        $this->usergroup = $usergroup;
+    }
+
+    public function indexAction()
+    {
+        $this->addControl(new UsergroupList([$this->usergroup]));
+
+        $this->addContent(new UserList($this->usergroup->user));
+    }
+}
