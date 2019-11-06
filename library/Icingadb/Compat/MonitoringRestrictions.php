4b9b562dd350fb58322e23f9e468508e555457ce
Johannes Meyer <johannes.meyer@icinga.com>
2019-11-06 15:39:47 +0100

    Introduce class `MonitoringRestrictions`

---
+++ library/Icingadb/Compat/MonitoringRestrictions.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Icinga\Module\Icingadb\Compat;
+
+use Icinga\Authentication\Auth;
+use Icinga\Data\Filter\Filter;
+use Icinga\Exception\ConfigurationError;
+use Icinga\Exception\QueryException;
+
+class MonitoringRestrictions
+{
+    /**
+     * Return restriction information for an eventually authenticated user
+     *
+     * @param   string  $name   Restriction name
+     *
+     * @return  array
+     */
+    protected static function getRestrictions($name)
+    {
+        return Auth::getInstance()->getRestrictions($name);
+    }
+
+    /**
+     * Get a restriction of the authenticated user
+     *
+     * @param   string $name        Name of the restriction
+     *
+     * @return  Filter              Filter object
+     * @throws  ConfigurationError  If the restriction contains invalid filter columns
+     */
+    public static function getRestriction($name)
+    {
+        $restriction = Filter::matchAny();
+        $restriction->setAllowedFilterColumns(array(
+            'host_name',
+            'hostgroup_name',
+            'instance_name',
+            'service_description',
+            'servicegroup_name',
+            function ($c) {
+                return preg_match('/^_(?:host|service)_/i', $c);
+            }
+        ));
+
+        foreach (self::getRestrictions($name) as $filter) {
+            if ($filter === '*') {
+                return Filter::matchAll();
+            }
+
+            try {
+                $restriction->addFilter(Filter::fromQueryString($filter));
+            } catch (QueryException $e) {
+                throw new ConfigurationError(
+                    mt(
+                        'monitoring',
+                        'Cannot apply restriction %s using the filter %s. You can only use the following columns: %s'
+                    ),
+                    $name,
+                    $filter,
+                    implode(', ', array(
+                        'instance_name',
+                        'host_name',
+                        'hostgroup_name',
+                        'service_description',
+                        'servicegroup_name',
+                        '_(host|service)_<customvar-name>'
+                    )),
+                    $e
+                );
+            }
+        }
+
+        if ($restriction->isEmpty()) {
+            return Filter::matchAll();
+        }
+
+        return $restriction;
+    }
+}
