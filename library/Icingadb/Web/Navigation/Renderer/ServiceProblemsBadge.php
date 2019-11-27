f77a440750184827d42a2b69d998e6f6ce0d63e7
Eric Lippmann <eric.lippmann@icinga.com>
2019-11-27 13:17:44 +0100

    Introduce new renderer for menu host and service problem badges

---
+++ library/Icingadb/Web/Navigation/Renderer/ServiceProblemsBadge.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Icinga\Module\Icingadb\Web\Navigation\Renderer;
+
+use Icinga\Module\Icingadb\Common\Links;
+use Icinga\Module\Icingadb\Model\ServicestateSummary;
+
+class ServiceProblemsBadge extends ProblemsBadge
+{
+    protected function fetchProblemsCount()
+    {
+        return ServicestateSummary::on($this->getDb())->with('state')->first()->services_critical_unhandled;
+    }
+
+    protected function getUrl()
+    {
+        return Links::services()->setParams(['service.state.is_problem' => 'y', 'sort' => 'service.state.severity desc']);
+    }
+}
