cfdd082cc7a8e507d071ced66beb188834eb20e2
Eric Lippmann <eric.lippmann@icinga.com>
2019-12-03 11:36:59 +0100

    Introduce widget HostStatusBar

---
+++ library/Icingadb/Widget/HostStatusBar.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Icinga\Module\Icingadb\Widget;
+
+use Icinga\Module\Icingadb\Common\BaseStatusBar;
+use ipl\Html\BaseHtmlElement;
+
+class HostStatusBar extends BaseStatusBar
+{
+    protected function assembleTotal(BaseHtmlElement $total)
+    {
+        $total->add(sprintf('%d Hosts:', $this->summary->hosts_total));
+    }
+
+    protected function createStateBadges()
+    {
+        return new HostStateBadges($this->summary);
+    }
+}
