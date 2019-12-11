742b0d6cc915384868cd56b7d5780164a2bf41af
Eric Lippmann <eric.lippmann@icinga.com>
2019-12-11 15:58:53 +0100

    Servicegroup: Use and adjust getDefaultSort() in favor of removed getSortRules()

--- library/Icingadb/Model/Servicegroup.php
+++ library/Icingadb/Model/Servicegroup.php
@@ -32,9 +32,9 @@ class Servicegroup extends Model
         ];
     }
 
-    public function getSortRules()
+    public function getDefaultSort()
     {
-        return ['display_name'];
+        return 'display_name';
     }
 
     public function createBehaviors(Behaviors $behaviors)
