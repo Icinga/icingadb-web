b20efe7641f913a541dab49649c18871e16438b2
Eric Lippmann <eric.lippmann@icinga.com>
2019-12-16 11:27:39 +0100

    Reintroduce DowntimeListItem

---
+++ library/Icingadb/Widget/DowntimeListItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Icinga\Module\Icingadb\Widget;
+
+use Icinga\Module\Icingadb\Common\ListItemCommonLayout;
+use Icinga\Module\Icingadb\Widget\ItemList\BaseDowntimeListItem;
+
+class DowntimeListItem extends BaseDowntimeListItem
+{
+    use ListItemCommonLayout;
+}
