d1501cc091df56150ffc42e5909625edb6a6a874
Eric Lippmann <eric.lippmann@icinga.com>
2019-12-16 11:14:24 +0100

    CommentList: Use trait CaptionDisabled

--- library/Icingadb/Widget/ItemList/CommentList.php
+++ library/Icingadb/Widget/ItemList/CommentList.php
@@ -2,11 +2,14 @@
 
 namespace Icinga\Module\Icingadb\Widget\ItemList;
 
+use Icinga\Module\Icingadb\Common\CaptionDisabled;
 use Icinga\Module\Icingadb\Common\Links;
 use Icinga\Module\Icingadb\Widget\BaseItemList;
 
 class CommentList extends BaseItemList
 {
+    use CaptionDisabled;
+
     protected $defaultAttributes = ['class' => 'comment-list'];
 
     protected function getItemClass()
