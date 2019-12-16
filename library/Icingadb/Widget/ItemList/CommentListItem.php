b0dcb104dcd6231dc18db6bebedc44ef70b390c1
Eric Lippmann <eric.lippmann@icinga.com>
2019-12-16 11:02:13 +0100

    Reintroduce CommentListItem

---
+++ library/Icingadb/Widget/ItemList/CommentListItem.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Icinga\Module\Icingadb\Widget\ItemList;
+
+use Icinga\Module\Icingadb\Common\ListItemCommonLayout;
+
+class CommentListItem extends BaseCommentListItem
+{
+    use ListItemCommonLayout;
+}
