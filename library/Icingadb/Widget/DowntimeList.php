dc8b96b19759a02404461dbff60bb6bd91b8935f
Eric Lippmann <eric.lippmann@icinga.com>
2019-12-16 11:28:35 +0100

    DowntimeList: Use and make use of trait ViewMode

--- library/Icingadb/Widget/DowntimeList.php
+++ library/Icingadb/Widget/DowntimeList.php
@@ -3,15 +3,26 @@
 namespace Icinga\Module\Icingadb\Widget;
 
 use Icinga\Module\Icingadb\Common\CaptionDisabled;
+use Icinga\Module\Icingadb\Common\ViewMode;
+use Icinga\Module\Icingadb\Widget\ItemList\DowntimeListItemMinimal;
 
 class DowntimeList extends BaseItemList
 {
     use CaptionDisabled;
+    use ViewMode;
 
     protected $defaultAttributes = ['class' => 'downtime-list'];
 
     protected function getItemClass()
     {
+        $viewMode = $this->getViewMode();
+
+        $this->addAttributes(['class' => $viewMode]);
+
+        if ($viewMode === 'minimal') {
+            return DowntimeListItemMinimal::class;
+        }
+
         return DowntimeListItem::class;
     }
 }
