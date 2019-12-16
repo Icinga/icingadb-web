12ba11cc68f987161b69791a0cd9a07e2aa1558f
Eric Lippmann <eric.lippmann@icinga.com>
2019-12-16 14:27:29 +0100

    CommentDetail: Remove obsolete methods

--- library/Icingadb/Widget/Detail/CommentDetail.php
+++ library/Icingadb/Widget/Detail/CommentDetail.php
@@ -5,10 +5,8 @@ namespace Icinga\Module\Icingadb\Widget\Detail;
 use Icinga\Module\Icingadb\Common\Auth;
 use Icinga\Module\Icingadb\Common\HostLink;
 use Icinga\Module\Icingadb\Common\HostLinks;
-use Icinga\Module\Icingadb\Common\Icons;
 use Icinga\Module\Icingadb\Common\ServiceLink;
 use Icinga\Module\Icingadb\Common\ServiceLinks;
-use Icinga\Module\Icingadb\Widget\TimeAgo;
 use Icinga\Module\Icingadb\Widget\TimeUntil;
 use Icinga\Module\Monitoring\Forms\Command\Object\DeleteCommentCommandForm;
 use ipl\Html\BaseHtmlElement;
@@ -32,16 +30,6 @@ class CommentDetail extends BaseHtmlElement
     public function __construct($comment)
     {
         $this->comment = $comment;
-    }
-
-    public function getControl()
-    {
-        return Html::tag('ul', ['class' => 'comment-detail item-list'], [
-            Html::tag('li', ['class' => 'list-item'], [
-                $this->createVisual(),
-                $this->createMain()
-            ])
-        ]);
     }
 
     protected function createComment()
@@ -74,52 +62,6 @@ class CommentDetail extends BaseHtmlElement
         return $details;
     }
 
-    protected function createMain()
-    {
-        $title = Html::tag('div', ['class' => 'title']);
-        $header = Html::tag('header', $title);
-        $main = Html::tag('div', ['class' => 'main'], $header);
-
-        $isAck = $this->comment->entry_type === 'ack';
-        $expires = $this->comment->expire_time;
-
-        $title->add([
-            new Icon(Icons::USER),
-            $this->comment->author,
-            ' ',
-            ($isAck ? 'acknowledged' : 'commented'),
-            ' '
-        ]);
-
-        if ($this->comment->object_type === 'host') {
-            $link = $this->createHostLink($this->comment->host, true);
-        } else {
-            $link = $this->createServiceLink($this->comment->service, $this->comment->service->host, true);
-        }
-
-        $title->add([
-            $link,
-            ' ',
-            new TimeAgo($this->comment->entry_time)
-        ]);
-
-        if ($isAck) {
-            $label = ['ack'];
-
-            if ($this->comment->is_persistent) {
-                array_unshift($label, new Icon(Icons::IS_PERSISTENT));
-            }
-
-            $title->add(HTML::tag('span', ['class' => 'ack-badge badge'], $label));
-        }
-
-        if ($expires != 0) {
-            $title->add(HTML::tag('span', ['class' => 'ack-badge badge'], 'EXPIRES'));
-        }
-
-        return $main;
-    }
-
     protected function createRemoveCommentForm()
     {
         $formData = [
@@ -148,15 +90,6 @@ class CommentDetail extends BaseHtmlElement
             ->render();
 
         return new HtmlString($removeCommentForm->render());
-    }
-
-    protected function createVisual()
-    {
-        return Html::tag(
-            'div',
-            ['class' => 'visual'],
-            Html::tag('div', ['class' => 'user-ball'], $this->comment->author[0])
-        );
     }
 
     protected function assemble()
