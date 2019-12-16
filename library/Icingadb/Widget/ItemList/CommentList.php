3fe5ed86d68a1e41253d957be89070e51f9ea194
Eric Lippmann <eric.lippmann@icinga.com>
2019-12-16 11:15:34 +0100

    CommentList: Use and make use of trait ViewMode

--- library/Icingadb/Widget/ItemList/CommentList.php
+++ library/Icingadb/Widget/ItemList/CommentList.php
@@ -4,16 +4,26 @@ namespace Icinga\Module\Icingadb\Widget\ItemList;
 
 use Icinga\Module\Icingadb\Common\CaptionDisabled;
 use Icinga\Module\Icingadb\Common\Links;
+use Icinga\Module\Icingadb\Common\ViewMode;
 use Icinga\Module\Icingadb\Widget\BaseItemList;
 
 class CommentList extends BaseItemList
 {
     use CaptionDisabled;
+    use ViewMode;
 
     protected $defaultAttributes = ['class' => 'comment-list'];
 
     protected function getItemClass()
     {
+        $viewMode = $this->getViewMode();
+
+        $this->addAttributes(['class' => $viewMode]);
+
+        if ($viewMode === 'minimal') {
+            return CommentListItemMinimal::class;
+        }
+
         return CommentListItem::class;
     }
 
