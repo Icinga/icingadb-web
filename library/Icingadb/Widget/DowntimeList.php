89f9d7bb78976c31bbe768b2c45d6a220ebf95dc
Eric Lippmann <eric.lippmann@icinga.com>
2019-12-16 11:26:33 +0100

    DowntimeList: Use trait CaptionDisabled

--- library/Icingadb/Widget/DowntimeList.php
+++ library/Icingadb/Widget/DowntimeList.php
@@ -2,8 +2,12 @@
 
 namespace Icinga\Module\Icingadb\Widget;
 
+use Icinga\Module\Icingadb\Common\CaptionDisabled;
+
 class DowntimeList extends BaseItemList
 {
+    use CaptionDisabled;
+
     protected $defaultAttributes = ['class' => 'downtime-list'];
 
     protected function getItemClass()
