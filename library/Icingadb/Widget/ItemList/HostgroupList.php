01be5f6a3a86101edd1d8cfc60858295fcb97551
Eric Lippmann <eric.lippmann@icinga.com>
2019-12-18 14:31:06 +0100

    HostgroupList: Use and make use of ViewMode

--- library/Icingadb/Widget/ItemList/HostgroupList.php
+++ library/Icingadb/Widget/ItemList/HostgroupList.php
@@ -2,14 +2,19 @@
 
 namespace Icinga\Module\Icingadb\Widget\ItemList;
 
+use Icinga\Module\Icingadb\Common\ViewMode;
 use Icinga\Module\Icingadb\Widget\BaseItemList;
 
 class HostgroupList extends BaseItemList
 {
+    use ViewMode;
+
     protected $defaultAttributes = ['class' => 'hostgroup-list item-table'];
 
     protected function getItemClass()
     {
+        $this->addAttributes(['class' => $this->getViewMode()]);
+
         return HostgroupListItem::class;
     }
 }
