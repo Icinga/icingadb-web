bce0c06b17d44538045402493125fd393ec3926b
Johannes Meyer <johannes.meyer@icinga.com>
2020-03-03 08:12:08 +0100

    tactical/index: Add missing details and filter capabilities

--- application/controllers/TacticalController.php
+++ application/controllers/TacticalController.php
@@ -3,6 +3,7 @@
 namespace Icinga\Module\Icingadb\Controllers;
 
 use Icinga\Chart\Donut;
+use Icinga\Module\Icingadb\Common\Links;
 use Icinga\Module\Icingadb\Model\HoststateSummary;
 use Icinga\Module\Icingadb\Model\ServicestateSummary;
 use Icinga\Module\Icingadb\Web\Controller;
@@ -20,9 +21,18 @@ class TacticalController extends Controller
         $db = $this->getDb();
 
         $hoststateSummary = HoststateSummary::on($db)->with('state');
-        $servicestateSummary = ServicestateSummary::on($db)->with('state');
+        // With relation `host` because otherwise the filter editor only presents service cols
+        $servicestateSummary = ServicestateSummary::on($db)->with(['state', 'host']);
+
+        $filterControl = $this->createFilterControl($servicestateSummary);
+        $this->createFilterControl($hoststateSummary);
+
+        $this->filter($hoststateSummary);
+        $this->filter($servicestateSummary);
 
         yield $this->export($hoststateSummary, $servicestateSummary);
+
+        $this->addControl($filterControl);
 
         $hoststateSummary = $hoststateSummary->first();
         $servicestateSummary = $servicestateSummary->first();
@@ -31,8 +41,15 @@ class TacticalController extends Controller
             ->addSlice($hoststateSummary->hosts_up, ['class' => 'slice-state-ok'])
             ->addSlice($hoststateSummary->hosts_down_handled, ['class' => 'slice-state-critical-handled'])
             ->addSlice($hoststateSummary->hosts_down_unhandled, ['class' => 'slice-state-critical'])
+            ->addSlice($hoststateSummary->hosts_unreachable_handled, ['class' => 'slice-state-unreachable-handled'])
+            ->addSlice($hoststateSummary->hosts_unreachable_unhandled, ['class' => 'slice-state-unreachable'])
             ->addSlice($hoststateSummary->hosts_pending, ['class' => 'slice-state-pending'])
             ->setLabelBig($hoststateSummary->hosts_down_unhandled)
+            ->setLabelBigUrl(Links::hosts()->addFilter($this->getFilter())->addParams([
+                'host.state.soft_state' => 1,
+                'host.state.is_handled' => 'n',
+                'sort' => 'host.state.last_state_change'
+            ]))
             ->setLabelBigEyeCatching($hoststateSummary->hosts_down_unhandled > 0)
             ->setLabelSmall($this->translate('Hosts Down'));
 
@@ -46,19 +63,26 @@ class TacticalController extends Controller
             ->addSlice($servicestateSummary->services_unknown_unhandled, ['class' => 'slice-state-unknown'])
             ->addSlice($servicestateSummary->services_pending, ['class' => 'slice-state-pending'])
             ->setLabelBig($servicestateSummary->services_critical_unhandled)
+            ->setLabelBigUrl(Links::services()->addFilter($this->getFilter())->addParams([
+                'service.state.soft_state' => 2,
+                'service.state.is_handled' => 'n',
+                'sort' => 'service.state.last_state_change'
+            ]))
             ->setLabelBigEyeCatching($servicestateSummary->services_critical_unhandled > 0)
             ->setLabelSmall($this->translate('Services Critical'));
 
-        $this->addContent(Html::tag('section', ['class' => 'donut-container'], [
-                Html::tag('h2', 'Host Summary'),
-                Html::tag('div', ['class' => 'donut'], new HtmlString($hostsChart->render())),
-                new HostStateBadges($hoststateSummary)
+        $this->addContent(Html::tag('section', ['class' => 'donut-container', 'data-base-target' => '_next'], [
+            Html::tag('h2', 'Host Summary'),
+            Html::tag('div', ['class' => 'donut'], new HtmlString($hostsChart->render())),
+            (new HostStateBadges($hoststateSummary))->setBaseFilter($this->getFilter())
         ]));
 
-        $this->addContent(Html::tag('section', ['class' => 'donut-container'], [
-                Html::tag('h2', 'Service Summary'),
-                Html::tag('div', ['class' => 'donut'], new HtmlString($servicesChart->render())),
-                new ServiceStateBadges($servicestateSummary)
+        $this->addContent(Html::tag('section', ['class' => 'donut-container', 'data-base-target' => '_next'], [
+            Html::tag('h2', 'Service Summary'),
+            Html::tag('div', ['class' => 'donut'], new HtmlString($servicesChart->render())),
+            (new ServiceStateBadges($servicestateSummary))->setBaseFilter($this->getFilter())
         ]));
+
+        $this->setAutorefreshInterval(10);
     }
 }
