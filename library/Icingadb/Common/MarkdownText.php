d061de728fb8518808e89700b3ee3a4cdd044eff
Johannes Meyer <johannes.meyer@icinga.com>
2020-03-31 17:32:29 +0200

    Introduce class `MarkdownText`

---
+++ library/Icingadb/Common/MarkdownText.php
@@ -0,0 +1,28 @@
+<?php
+
+/* Icinga DB Web | (c) 2020 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Icingadb\Common;
+
+use Icinga\Web\Helper\Markdown;
+use ipl\Html\BaseHtmlElement;
+use ipl\Html\DeferredText;
+
+class MarkdownText extends BaseHtmlElement
+{
+    protected $tag = 'section';
+
+    protected $defaultAttributes = ['class' => 'markdown'];
+
+    /**
+     * MarkdownText constructor.
+     *
+     * @param string $text
+     */
+    public function __construct($text)
+    {
+        $this->add((new DeferredText(function () use ($text) {
+            return Markdown::text($text);
+        }))->setEscaped(true));
+    }
+}
