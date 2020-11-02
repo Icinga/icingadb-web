07f78b507d81ca67278dd03967610c3d37d43257
Johannes Meyer <johannes.meyer@icinga.com>
2020-11-02 14:32:47 +0100

    tactical/: Show search bar control

--- application/controllers/TacticalController.php
+++ application/controllers/TacticalController.php
@@ -4,11 +4,15 @@
 
 namespace Icinga\Module\Icingadb\Controllers;
 
+use GuzzleHttp\Psr7\ServerRequest;
+use Icinga\Data\Filter\Filter;
 use Icinga\Module\Icingadb\Model\HoststateSummary;
 use Icinga\Module\Icingadb\Model\ServicestateSummary;
+use Icinga\Module\Icingadb\Web\Control\SearchBar\ObjectSuggestions;
 use Icinga\Module\Icingadb\Web\Controller;
 use Icinga\Module\Icingadb\Widget\HostSummaryDonut;
 use Icinga\Module\Icingadb\Widget\ServiceSummaryDonut;
+use ipl\Web\Filter\QueryString;
 
 class TacticalController extends Controller
 {
@@ -22,26 +26,41 @@ class TacticalController extends Controller
         // With relation `host` because otherwise the filter editor only presents service cols
         $servicestateSummary = ServicestateSummary::on($db)->with(['state', 'host']);
 
-        $filterControl = $this->createFilterControl($servicestateSummary);
-        $this->createFilterControl($hoststateSummary);
+        $this->handleSearchRequest($servicestateSummary);
 
-        $this->filter($hoststateSummary);
-        $this->filter($servicestateSummary);
+        $searchBar = $this->createSearchBar($servicestateSummary);
+
+        $filter = $searchBar->getFilter();
+
+        $this->filter($hoststateSummary, $filter);
+        $this->filter($servicestateSummary, $filter);
 
         yield $this->export($hoststateSummary, $servicestateSummary);
 
-        $this->addControl($filterControl);
+        $this->addControl($searchBar);
 
         $this->addContent(
             (new HostSummaryDonut($hoststateSummary->first()))
-                ->setBaseFilter($this->getFilter())
+                ->setBaseFilter(Filter::fromQueryString(QueryString::render($filter)))
         );
 
         $this->addContent(
             (new ServiceSummaryDonut($servicestateSummary->first()))
-                ->setBaseFilter($this->getFilter())
+                ->setBaseFilter(Filter::fromQueryString(QueryString::render($filter)))
         );
 
+        if ($searchBar->hasBeenSent()) {
+            $this->sendMultipartUpdate();
+        }
+
         $this->setAutorefreshInterval(10);
+    }
+
+    public function completeAction()
+    {
+        $suggestions = new ObjectSuggestions();
+        $suggestions->setModel(ServicestateSummary::class);
+        $suggestions->forRequest(ServerRequest::fromGlobals());
+        $this->getDocument()->add($suggestions);
     }
 }
