8731477ec8a6649e454ef8d5af7171f654a2f181
Johannes Meyer <johannes.meyer@icinga.com>
2021-02-08 14:28:00 +0100

    ConfigController: Drop usage of the monitoring command transport UI

--- application/controllers/ConfigController.php
+++ application/controllers/ConfigController.php
@@ -4,22 +4,15 @@
 
 namespace Icinga\Module\Icingadb\Controllers;
 
-use Exception;
 use Icinga\Application\Config;
-use Icinga\Exception\NotFoundError;
-use Icinga\Forms\ConfirmRemovalForm;
 use Icinga\Module\Icingadb\Forms\DatabaseConfigForm;
 use Icinga\Module\Icingadb\Forms\RedisConfigForm;
 use Icinga\Module\Icingadb\Web\Controller;
 use Icinga\Module\Monitoring\Forms\Config\SecurityConfigForm;
-use Icinga\Module\Monitoring\Forms\Config\TransportConfigForm;
-use Icinga\Module\Monitoring\Forms\Config\TransportReorderForm;
 use Icinga\Web\Form;
-use Icinga\Web\Notification;
 use Icinga\Web\Widget\Tab;
 use Icinga\Web\Widget\Tabs;
 use ipl\Html\HtmlString;
-use ipl\Web\Widget\ButtonLink;
 
 class ConfigController extends Controller
 {
@@ -29,142 +22,6 @@ class ConfigController extends Controller
 //
 //        parent::init();
 //    }
-
-    public function commandTransportsAction()
-    {
-        $form = new TransportReorderForm();
-
-        $form->handleRequest();
-
-        $this->mergeTabs($this->Module()->getConfigTabs()->activate('command-transports'));
-
-        $this->addControl(
-            (new ButtonLink(
-                t('Create Command Transport'),
-                'icingadb/config/create-command-transport',
-                'plus'
-            ))->setBaseTarget('_next')
-        );
-
-        $this->addFormToContent($form);
-    }
-
-    public function createCommandTransportAction()
-    {
-        $this->setTitle(t('Create Command Transport'));
-
-        $form = (new TransportConfigForm())
-            ->setIniConfig(Config::module('monitoring', 'commandtransports'))
-            ->setRedirectUrl('icingadb/config/command-transports');
-
-        $form->setOnSuccess(function (TransportConfigForm $form) {
-            try {
-                $form->add($form::transformEmptyValuesToNull($form->getValues()));
-            } catch (Exception $e) {
-                $form->error($e->getMessage());
-
-                return false;
-            }
-
-            if ($form->save()) {
-                Notification::success(t('Command transport successfully created'));
-
-                return true;
-            }
-
-            return false;
-        });
-
-        $form->handleRequest();
-
-        $this->addFormToContent($form);
-    }
-
-    public function deleteCommandTransportAction()
-    {
-        $this->setTitle(t('Delete Command Transport'));
-
-        $transportName = $this->params->getRequired('transport');
-
-        $transportConfigForm = (new TransportConfigForm())
-            ->setIniConfig(Config::module('monitoring', 'commandtransports'));
-
-        $confirmRemovalForm = (new ConfirmRemovalForm())
-            ->setRedirectUrl('icingadb/config/command-transports');
-
-        $confirmRemovalForm->setOnSuccess(
-            function (ConfirmRemovalForm $form) use ($transportName, $transportConfigForm) {
-                try {
-                    $transportConfigForm->delete($transportName);
-                } catch (Exception $e) {
-                    $form->error($e->getMessage());
-
-                    return false;
-                }
-
-                if ($transportConfigForm->save()) {
-                    Notification::success(sprintf(
-                        t('Command transport "%s" successfully removed'),
-                        $transportName
-                    ));
-
-                    return true;
-                }
-
-                return false;
-            }
-        );
-
-        $confirmRemovalForm->handleRequest();
-
-        $this->addFormToContent($confirmRemovalForm);
-    }
-
-    public function updateCommandTransportAction()
-    {
-        $this->setTitle(t('Update Command Transport'));
-
-        $transportName = $this->params->getRequired('transport');
-
-        $form = (new TransportConfigForm())
-            ->setIniConfig(Config::module('monitoring', 'commandtransports'))
-            ->setRedirectUrl('icingadb/config/command-transports');
-
-        $form->setOnSuccess(function (TransportConfigForm $form) use ($transportName) {
-            try {
-                $form->edit($transportName, array_map(
-                    function ($v) {
-                        return $v !== '' ? $v : null;
-                    },
-                    $form->getValues()
-                ));
-            } catch (Exception $e) {
-                $form->error($e->getMessage());
-
-                return false;
-            }
-
-            if ($form->save()) {
-                Notification::success(sprintf(
-                    t('Command transport "%s" successfully updated'),
-                    $transportName
-                ));
-
-                return true;
-            }
-
-            return false;
-        });
-
-        try {
-            $form->load($transportName);
-            $form->handleRequest();
-        } catch (NotFoundError $_) {
-            $this->httpNotFound(sprintf(t('Command transport "%s" not found'), $transportName));
-        }
-
-        $this->addFormToContent($form);
-    }
 
     public function databaseAction()
     {
