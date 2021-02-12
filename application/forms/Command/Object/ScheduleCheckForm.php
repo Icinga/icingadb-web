e52918211c74a81cbba8d1c5a5999ff89192f982
Johannes Meyer <johannes.meyer@icinga.com>
2021-02-12 11:37:10 +0100

    Introduce class `ScheduleCheckForm`

---
+++ application/forms/Command/Object/ScheduleCheckForm.php
@@ -0,0 +1,81 @@
+<?php
+
+/* Icinga DB Web | (c) 2021 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Icingadb\Forms\Command\Object;
+
+use DateInterval;
+use DateTime;
+use Icinga\Module\Icingadb\Command\Object\ScheduleCheckCommand;
+use Icinga\Module\Icingadb\Forms\Command\CommandForm;
+use ipl\Html\HtmlElement;
+use ipl\Orm\Model;
+use ipl\Web\FormDecorator\IcingaFormDecorator;
+use ipl\Web\Widget\Icon;
+
+class ScheduleCheckForm extends CommandForm
+{
+    protected function assembleElements()
+    {
+        $this->add(new HtmlElement('div', ['class' => 'form-description'], [
+            new Icon('info-circle', ['class' => 'form-description-icon']),
+            new HtmlElement('ul', null, [
+                new HtmlElement('li', null, t(
+                    'This command is used to schedule the next check of hosts or services. Icinga'
+                    . ' will re-queue the hosts or services to be checked at the time you specify.'
+                ))
+            ])
+        ]));
+
+        $decorator = new IcingaFormDecorator();
+
+        $this->addElement(
+            'localDateTime',
+            'check_time',
+            [
+                'required'      => true,
+                'label'         => t('Check Time'),
+                'description'   => t('Set the date and time when the check should be scheduled.'),
+                'value'         => (new DateTime())->add(new DateInterval('PT1H'))
+            ]
+        );
+        $decorator->decorate($this->getElement('check_time'));
+
+        $this->addElement(
+            'checkbox',
+            'force_check',
+            [
+                'label'         => t('Force Check'),
+                'description'   => t(
+                    'If you select this option, Icinga will force a check regardless of both what time the'
+                    . ' scheduled check occurs and whether or not checks are enabled.'
+                )
+            ]
+        );
+        $decorator->decorate($this->getElement('force_check'));
+    }
+
+    protected function assembleSubmitButton()
+    {
+        $this->addElement(
+            'submit',
+            'btn_submit',
+            [
+                'required'  => true,
+                'label'     => tp('Schedule check', 'Schedule checks', count($this->getObjects()))
+            ]
+        );
+
+        (new IcingaFormDecorator())->decorate($this->getElement('btn_submit'));
+    }
+
+    protected function getCommand(Model $object)
+    {
+        $command = new ScheduleCheckCommand();
+        $command->setObject($object);
+        $command->setForced($this->getElement('force_check')->isChecked());
+        $command->setCheckTime($this->getValue('check_time')->getTimestamp());
+
+        return $command;
+    }
+}
