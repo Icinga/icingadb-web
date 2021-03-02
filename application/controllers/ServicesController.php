76cb944becd0267731c0012ca7cc5ce54a36881a
Johannes Meyer <johannes.meyer@icinga.com>
2021-03-02 16:56:34 +0100

    services/search-editor: Set a proper title

--- application/controllers/ServicesController.php
+++ application/controllers/ServicesController.php
@@ -194,6 +194,7 @@ class ServicesController extends Controller
         ]);
 
         $this->getDocument()->add($this->createSearchEditor($services));
+        $this->setTitle(t('Adjust Filter'));
     }
 
     public function fetchCommandTargets()
