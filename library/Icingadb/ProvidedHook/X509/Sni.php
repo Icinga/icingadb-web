d1c09aa70b8659987376fb8e4ac292d892dc1ae3
Ravi Kumar Kempapura Srinivasa <ravi.srinivasa@icinga.com>
2021-03-17 15:43:03 +0100

    Introduce implementation for hook X509/Sni in icingadb
    The hook X509/Sni is implemented for icingadb in this commit.

---
+++ library/Icingadb/ProvidedHook/X509/Sni.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Icinga\Module\Icingadb\ProvidedHook\X509;
+
+use Icinga\Data\Filter\Filter;
+use Icinga\Module\Icingadb\Common\Auth;
+use Icinga\Module\Icingadb\Common\Database;
+use Icinga\Module\Icingadb\Model\Host;
+use Icinga\Module\X509\Hook\SniHook;
+use ipl\Orm\Compat\FilterProcessor;
+use ipl\Web\Filter\QueryString;
+
+class Sni extends SniHook
+{
+    use Auth;
+    use Database;
+
+    /**
+     * @inheritDoc
+     */
+    public function getHosts(Filter $filter = null)
+    {
+        $queryHost = Host::on($this->getDb());
+
+        $queryHost->getSelectBase();
+
+        $hostStatusCols = [
+            'host_name'      => 'name',
+            'host_address'   => 'address',
+            'host_address6'  => 'address6'
+        ];
+
+        $queryHost = $queryHost->columns($hostStatusCols);
+
+        $this->applyRestrictions($queryHost);
+
+        $filter = Filter::where('name', 'dummy-1');
+        if ($filter !== null) {
+            $queryString = $filter->toQueryString();
+            $filterCondition = QueryString::parse($queryString);
+            FilterProcessor::apply($filterCondition, $queryHost);
+        }
+
+        $hosts = $this->getdb()->select($queryHost->assembleSelect());
+
+        foreach ($hosts as $host) {
+            if (! empty($host->host_address)) {
+                yield $host->host_address => $host->host_name;
+            }
+
+            if (! empty($host->host_address6)) {
+                yield $host->host_address6 => $host->host_name;
+            }
+        }
+    }
+}
