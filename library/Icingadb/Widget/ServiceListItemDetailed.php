8bea9714518663c4784106277da02765f008f2d4
Florian Strohmaier <florian.strohmaier@me.com>
2021-03-23 13:28:52 +0100

    ServiceListItemDetailed: Add footer element
    refs #160

--- library/Icingadb/Widget/ServiceListItemDetailed.php
+++ library/Icingadb/Widget/ServiceListItemDetailed.php
@@ -4,11 +4,26 @@
 
 namespace Icinga\Module\Icingadb\Widget;
 
+use Icinga\Module\Icingadb\Common\ListItemDetailedLayout;
 use Icinga\Module\Icingadb\Compat\CompatPluginOutput;
 use ipl\Html\BaseHtmlElement;
+use ipl\Html\Html;
+use ipl\Web\Widget\StateBall;
 
-class ServiceListItemDetailed extends ServiceListItem
+class ServiceListItemDetailed extends BaseServiceListItem
 {
+    use ListItemDetailedLayout;
+
+    protected function getStateBallSize()
+    {
+        return StateBall::SIZE_LARGE;
+    }
+
+    protected function assembleFooter(BaseHtmlElement $footer)
+    {
+        $footer->add(Html::tag('p', [], 'Footer Service'));
+    }
+
     protected function assembleCaption(BaseHtmlElement $caption)
     {
         $caption->add(CompatPluginOutput::getInstance()->render(
