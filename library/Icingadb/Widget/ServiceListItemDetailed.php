173bcc002d05b20923aabd93789aa950d7d6486d
Florian Strohmaier <florian.strohmaier@me.com>
2021-03-25 16:01:07 +0100

    ServiceListItemDetailed: Use actual data for footer pies
    refs #160

--- library/Icingadb/Widget/ServiceListItemDetailed.php
+++ library/Icingadb/Widget/ServiceListItemDetailed.php
@@ -6,13 +6,20 @@ namespace Icinga\Module\Icingadb\Widget;
 
 use Icinga\Module\Icingadb\Common\ListItemDetailedLayout;
 use Icinga\Module\Icingadb\Compat\CompatPluginOutput;
+use Icinga\Module\Icingadb\Util\PerfdataSet;
 use ipl\Html\BaseHtmlElement;
 use ipl\Html\Html;
+use ipl\Html\HtmlElement;
+use ipl\Html\HtmlString;
+use ipl\Web\Widget\Icon;
 use ipl\Web\Widget\StateBall;
 
 class ServiceListItemDetailed extends BaseServiceListItem
 {
     use ListItemDetailedLayout;
+
+    /** @var int Max pie charts to be shown */
+    const PIE_CHART_LIMIT = 5;
 
     protected function getStateBallSize()
     {
@@ -21,7 +28,54 @@ class ServiceListItemDetailed extends BaseServiceListItem
 
     protected function assembleFooter(BaseHtmlElement $footer)
     {
-        $footer->add(Html::tag('p', [], 'Footer Service'));
+        $statusIcons = new HtmlElement('div', ['class' => 'status-icons']);
+
+        // ToDo(fs): Get `has_comments` from database
+        if ($this->item->comment->limit(1)->execute()->hasResult()) {
+            $statusIcons->add(new Icon('comments', ['title' => t('This item has been commented')]));
+        }
+
+        if (! $this->item->notifications_enabled) {
+            $statusIcons->add(new Icon('bell-slash', ['title' => t('Notifications disabled')]));
+        }
+
+        if (! $this->item->active_checks_enabled) {
+            $statusIcons->add(new Icon('eye-slash', ['title' => t('Active checks disabled')]));
+        }
+
+        $performanceData = new HtmlElement('div', ['class' => 'performance-data']);
+        if ($this->item->state->performance_data) {
+            $pieChartData = PerfdataSet::fromString($this->item->state->performance_data)->asArray();
+
+            $pies = [];
+            foreach ($pieChartData as $i => $perfdata) {
+                if ($perfdata->isVisualizable()) {
+                    $pies[] = $perfdata->asInlinePie()->render();
+                }
+
+                // Check if number of visualizable pie charts is larger than PIE_CHART_LIMIT
+                if (count($pies) > ServiceListItemDetailed::PIE_CHART_LIMIT) {
+                    break;
+                }
+            }
+
+            $maxVisiblePies = ServiceListItemDetailed::PIE_CHART_LIMIT - 2;
+            $numOfPies = count($pies);
+            foreach ($pies as $i => $pie) {
+                if (
+                    // Show max. 5 elements: if there are more than 5, show 4 + `…`
+                    $i > $maxVisiblePies && $numOfPies > ServiceListItemDetailed::PIE_CHART_LIMIT
+                ) {
+                    $performanceData->add(new HtmlElement('span', [], '…'));
+                    break;
+                }
+
+                $performanceData->add(HtmlString::create($pie));
+            }
+        }
+
+        $footer->add($statusIcons);
+        $footer->add($performanceData);
     }
 
     protected function assembleCaption(BaseHtmlElement $caption)
