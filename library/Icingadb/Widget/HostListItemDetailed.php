1188d28d12c129b94bd194504ab3d4ae00330127
Florian Strohmaier <florian.strohmaier@me.com>
2021-03-25 16:00:53 +0100

    HostListItemDetailed: Use actual data for footer pies
    refs #160

--- library/Icingadb/Widget/HostListItemDetailed.php
+++ library/Icingadb/Widget/HostListItemDetailed.php
@@ -6,8 +6,9 @@ namespace Icinga\Module\Icingadb\Widget;
 
 use Icinga\Module\Icingadb\Common\ListItemDetailedLayout;
 use Icinga\Module\Icingadb\Compat\CompatPluginOutput;
+use Icinga\Module\Icingadb\Util\PerfdataSet;
 use ipl\Html\BaseHtmlElement;
-use ipl\Html\Html;
+use ipl\Html\HtmlElement;
 use ipl\Html\HtmlString;
 use ipl\Web\Widget\Icon;
 use ipl\Web\Widget\StateBall;
@@ -16,7 +17,8 @@ class HostListItemDetailed extends BaseHostListItem
 {
     use ListItemDetailedLayout;
 
-    protected $pie;
+    /** @var int Max pie charts to be shown */
+    const PIE_CHART_LIMIT = 5;
 
     protected function getStateBallSize()
     {
@@ -25,56 +27,49 @@ class HostListItemDetailed extends BaseHostListItem
 
     protected function assembleFooter(BaseHtmlElement $footer)
     {
-        $this->pie = HtmlString::create('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" role="img" width="100%" height="100%" viewBox="0 0 1000 1000" preserveAspectRatio="xMinYMin meet" aria-labelledby="aria-title-pl aria-desc-pl">
-  <title id="aria-title-pl">pl 0% (0.00%)</title>
-  <desc id="aria-desc-pl">pl 0% (0.00%)</desc>
-  <g id="root" role="presentation">
-    <g x="0" y="0" id="root_inner" transform="translate(0.0, 0.0) scale(1.0, 1.0)">
-      <g id="outerGraph">
-        <g x="0" y="0" id="outerGraph_inner" transform="translate(15.0, -100.0) scale(1.2, 1.2)">
-          <g id="graph">
-            <g x="0" y="0" id="graph_inner" transform="translate(0.0, 0.0) scale(1.0, 1.0)">
-              <defs>
-                <clipPath id="clip">
-                  <rect x="1.0" y="0.0" width="1000.0" height="999.0" style="fill: none; stroke: #000;stroke-width: 0;;;"></rect>
-                </clipPath>
-              </defs>
-              <g>
-                <path d="M 400.0 100.0 A 400.0 400.0 0 1 1 399.6 100.0" style="fill: #ddccdd; stroke: #000;stroke-width: 1;;;" data-icinga-graph-type="pieslice"></path>
-              </g>
-              <g></g>
-            </g>
-          </g>
-        </g>
-      </g>
-    </g>
-  </g>
-</svg>');
-
-        $statusIcons = Html::tag('div', ['class' => 'status-icons']);
+        $statusIcons = new HtmlElement('div', ['class' => 'status-icons']);
 
         // ToDo(fs): Get `has_comments` from database
         if ($this->item->comment->limit(1)->execute()->hasResult()) {
-            $statusIcons->add(New Icon('comments', ['title' => t('This item has been commented')]));
+            $statusIcons->add(new Icon('comments', ['title' => t('This item has been commented')]));
         }
 
-        if (!$this->item->notifications_enabled) {
-            $statusIcons->add(New Icon('bell-slash', ['title' => t('Notifications Disabled')]));
+        if (! $this->item->notifications_enabled) {
+            $statusIcons->add(new Icon('bell-slash', ['title' => t('Notifications disabled')]));
         }
 
-        if (!$this->item->active_checks_enabled) {
-            $statusIcons->add(New Icon('eye-slash', ['title' => t('Active checks disabled')]));
+        if (! $this->item->active_checks_enabled) {
+            $statusIcons->add(new Icon('eye-slash', ['title' => t('Active checks disabled')]));
         }
 
-        $performanceData = Html::tag('div', ['class' => 'performance-data']);
+        $performanceData = new HtmlElement('div', ['class' => 'performance-data']);
         if ($this->item->state->performance_data) {
-            for ($i = 0; $i < 5; $i++) {
-                if ($i > 3) {
-                    $performanceData->add(Html::tag('span', [], '…'));
-                    break;
-                } else {
-                    $performanceData->add(Html::tag('div', ['class' => 'inline-pie'], $this->pie));
+            $pieChartData = PerfdataSet::fromString($this->item->state->performance_data)->asArray();
+
+            $pies = [];
+            foreach ($pieChartData as $i => $perfdata) {
+                if ($perfdata->isVisualizable()) {
+                    $pies[] = $perfdata->asInlinePie()->render();
                 }
+
+                // Check if number of visualizable pie charts is larger than PIE_CHART_LIMIT
+                if (count($pies) > HostListItemDetailed::PIE_CHART_LIMIT) {
+                    break;
+                }
+            }
+
+            $maxVisiblePies = HostListItemDetailed::PIE_CHART_LIMIT - 2;
+            $numOfPies = count($pies);
+            foreach ($pies as $i => $pie) {
+                if (
+                    // Show max. 5 elements: if there are more than 5, show 4 + `…`
+                    $i > $maxVisiblePies && $numOfPies > HostListItemDetailed::PIE_CHART_LIMIT
+                ) {
+                    $performanceData->add(new HtmlElement('span', [], '…'));
+                    break;
+                }
+
+                $performanceData->add(HtmlString::create($pie));
             }
         }
 
