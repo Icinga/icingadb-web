817c31bf08d0a5c1eaba2b8407638d766e937fe1
Johannes Meyer <johannes.meyer@icinga.com>
2021-06-21 17:05:03 +0200

    Setup/*Step: Use strict interfaces to create ipl\Html objects

--- library/Icingadb/Setup/DbResourceStep.php
+++ library/Icingadb/Setup/DbResourceStep.php
@@ -8,9 +8,11 @@ use Exception;
 use Icinga\Application\Config;
 use Icinga\Exception\IcingaException;
 use Icinga\Module\Setup\Step;
+use ipl\Html\Attributes;
 use ipl\Html\HtmlDocument;
 use ipl\Html\HtmlElement;
 use ipl\Html\Table;
+use ipl\Html\Text;
 
 class DbResourceStep extends Step
 {
@@ -54,74 +56,74 @@ class DbResourceStep extends Step
 
     public function getSummary()
     {
-        $description = new HtmlElement('p', null, mt(
+        $description = new HtmlElement('p', null, Text::create(mt(
             'icingadb',
             'Icinga DB will be accessed using the following connection details:'
-        ));
+        )));
 
         $resourceOptions = new Table();
-        $resourceOptions->add(Table::row([
-            new HtmlElement('strong', null, t('Host')),
+        $resourceOptions->addHtml(Table::row([
+            new HtmlElement('strong', null, Text::create(t('Host'))),
             $this->data['host']
         ]));
-        $resourceOptions->add(Table::row([
-            new HtmlElement('strong', null, t('Port')),
+        $resourceOptions->addHtml(Table::row([
+            new HtmlElement('strong', null, Text::create(t('Port'))),
             $this->data['port'] ?: ($this->data['db'] === 'mysql' ? 3306 : 5432)
         ]));
-        $resourceOptions->add(Table::row([
-            new HtmlElement('strong', null, t('Database')),
+        $resourceOptions->addHtml(Table::row([
+            new HtmlElement('strong', null, Text::create(t('Database'))),
             $this->data['dbname']
         ]));
-        $resourceOptions->add(Table::row([
-            new HtmlElement('strong', null, t('Username')),
+        $resourceOptions->addHtml(Table::row([
+            new HtmlElement('strong', null, Text::create(t('Username'))),
             $this->data['username']
         ]));
-        $resourceOptions->add(Table::row([
-            new HtmlElement('strong', null, t('Password')),
+        $resourceOptions->addHtml(Table::row([
+            new HtmlElement('strong', null, Text::create(t('Password'))),
             str_repeat('*', strlen($this->data['password']))
         ]));
-        $resourceOptions->add(Table::row([
-            new HtmlElement('strong', null, t('Charset')),
+        $resourceOptions->addHtml(Table::row([
+            new HtmlElement('strong', null, Text::create(t('Charset'))),
             $this->data['charset']
         ]));
 
         if (isset($this->data['use_ssl']) && $this->data['use_ssl']) {
-            $resourceOptions->add(Table::row([
-                new HtmlElement('strong', null, t('SSL Do Not Verify Server Certificate')),
+            $resourceOptions->addHtml(Table::row([
+                new HtmlElement('strong', null, Text::create(t('SSL Do Not Verify Server Certificate'))),
                 isset($this->data['ssl_do_not_verify_server_cert']) && $this->data['ssl_do_not_verify_server_cert']
                     ? t('Yes')
                     : t('No')
             ]));
-            $resourceOptions->add(Table::row([
-                new HtmlElement('strong', null, t('SSL Key')),
+            $resourceOptions->addHtml(Table::row([
+                new HtmlElement('strong', null, Text::create(t('SSL Key'))),
                 $this->data['ssl_key'] ?: mt('icingadb', 'None', 'non-existence of a value')
             ]));
-            $resourceOptions->add(Table::row([
-                new HtmlElement('strong', null, t('SSL Certificate')),
+            $resourceOptions->addHtml(Table::row([
+                new HtmlElement('strong', null, Text::create(t('SSL Certificate'))),
                 $this->data['ssl_cert'] ?: mt('icingadb', 'None', 'non-existence of a value')
             ]));
-            $resourceOptions->add(Table::row([
-                new HtmlElement('strong', null, t('SSL CA')),
+            $resourceOptions->addHtml(Table::row([
+                new HtmlElement('strong', null, Text::create(t('SSL CA'))),
                 $this->data['ssl_ca'] ?: mt('icingadb', 'None', 'non-existence of a value')
             ]));
-            $resourceOptions->add(Table::row([
-                new HtmlElement('strong', null, t('The CA certificate file path')),
+            $resourceOptions->addHtml(Table::row([
+                new HtmlElement('strong', null, Text::create(t('The CA certificate file path'))),
                 $this->data['ssl_capath'] ?: mt('icingadb', 'None', 'non-existence of a value')
             ]));
-            $resourceOptions->add(Table::row([
-                new HtmlElement('strong', null, t('SSL CA Path')),
+            $resourceOptions->addHtml(Table::row([
+                new HtmlElement('strong', null, Text::create(t('SSL CA Path'))),
                 $this->data['ssl_cipher'] ?: mt('icingadb', 'None', 'non-existence of a value')
             ]));
         }
 
-        $topic = new HtmlElement('div', ['class' => 'topic']);
-        $topic->add([$description, $resourceOptions]);
+        $topic = new HtmlElement('div', Attributes::create(['class' => 'topic']));
+        $topic->addHtml($description, $resourceOptions);
 
         $summary = new HtmlDocument();
-        $summary->add([
-            new HtmlElement('h2', null, mt('icingadb', 'Icinga DB Resource')),
+        $summary->addHtml(
+            new HtmlElement('h2', null, Text::create(mt('icingadb', 'Icinga DB Resource'))),
             $topic
-        ]);
+        );
 
         return $summary->render();
     }
