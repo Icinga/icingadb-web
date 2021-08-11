675e1185949e6b60075109ea014ed25e86359931
Florian Strohmaier <florian.strohmaier@icinga.com>
2021-08-11 12:41:08 +0200

    StateList: Move `$hasIconImages` property to `BaseItemList`
    refs #225

--- library/Icingadb/Widget/StateList.php
+++ library/Icingadb/Widget/StateList.php
@@ -12,29 +12,6 @@ abstract class StateList extends BaseItemList
     use ViewMode;
     use NoSubjectLink;
 
-    /** @var bool Whether the list contains at least one item with an icon_image */
-    protected $hasIconImages = false;
-
-    /**
-     * Get whether the list contains at least one item with an icon_image
-     *
-     * @return bool
-     */
-    public function hasIconImages()
-    {
-        return $this->hasIconImages;
-    }
-
-    /**
-     * Set whether the list contains at least one item with an icon_image
-     *
-     * @param bool $hasIconImages
-     */
-    public function setHasIconImages(bool $hasIconImages)
-    {
-        $this->hasIconImages = $hasIconImages;
-    }
-
     protected function assemble()
     {
         $this->addAttributes(['class' => $this->getViewMode()]);
