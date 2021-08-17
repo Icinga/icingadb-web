946c4eb67d7da106fa766c90f064495bb2f0057a
Johannes Meyer <johannes.meyer@icinga.com>
2021-08-17 17:24:47 +0200

    Introduce tab hook

---
+++ library/Icingadb/Hook/TabHook/HookActions.php
@@ -0,0 +1,139 @@
+<?php
+
+/* Icinga DB Web | (c) 2021 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Icingadb\Hook\TabHook;
+
+use Exception;
+use Generator;
+use Icinga\Application\Logger;
+use Icinga\Module\Icingadb\Hook\TabHook;
+use Icinga\Web\Hook;
+use ipl\Html\ValidHtml;
+use ipl\Orm\Model;
+use ipl\Stdlib\Str;
+
+/**
+ * Trait HookActions
+ *
+ * @method ValidHtml[] getDefaultTabControls() Get default control elements
+ */
+trait HookActions
+{
+    /** @var Model The object to load tabs for */
+    protected $objectToLoadTabsFor;
+
+    /** @var TabHook[] Loaded tab hooks */
+    protected $tabHooks;
+
+    public function __call($methodName, $args)
+    {
+        if (substr($methodName, -6) === 'Action') {
+            $hookName = substr($methodName, 0, -6);
+
+            $hooks = $this->loadTabHooks();
+            if (isset($hooks[$hookName])) {
+                $this->showTabHook($hooks[$hookName]);
+                return;
+            }
+        }
+
+        parent::__call($methodName, $args);
+    }
+
+    /**
+     * Register the object for which to load additional tabs
+     *
+     * @param Model $object
+     */
+    protected function loadTabsForObject(Model $object)
+    {
+        $this->objectToLoadTabsFor = $object;
+    }
+
+    /**
+     * Load tab hooks
+     *
+     * @return array<string, TabHook>
+     */
+    protected function loadTabHooks(): array
+    {
+        if ($this->objectToLoadTabsFor === null) {
+            return [];
+        } elseif ($this->tabHooks !== null) {
+            return $this->tabHooks;
+        }
+
+        $this->tabHooks = [];
+        foreach (Hook::all('Icingadb\\Tab') as $hook) {
+            /** @var TabHook $hook */
+            try {
+                if ($hook->shouldBeShown($this->objectToLoadTabsFor)) {
+                    $this->tabHooks[Str::camel($hook->getName())] = $hook;
+                }
+            } catch (Exception $e) {
+                Logger::error("Failed to load tab hook: %s\n%s", $e, $e->getTraceAsString());
+            }
+        }
+
+        return $this->tabHooks;
+    }
+
+    /**
+     * Load additional tabs
+     *
+     * @return Generator<string, array{label: string, url: string}>
+     */
+    protected function loadAdditionalTabs(): Generator
+    {
+        foreach ($this->loadTabHooks() as $hook) {
+            yield $hook->getName() => [
+                'label' => $hook->getLabel(),
+                'url'   => 'icingadb/' . $this->getRequest()->getControllerName() . '/' . $hook->getName()
+            ];
+        }
+    }
+
+    /**
+     * Render the given tab hook
+     *
+     * @param TabHook $hook
+     */
+    protected function showTabHook(TabHook $hook)
+    {
+        $moduleName = $hook->getModule()->getName();
+
+        foreach ($hook->getControls($this->objectToLoadTabsFor) as $control) {
+            $this->addControl($control);
+        }
+
+        if (! empty($this->controls->getContent())) {
+            $this->controls->addAttributes([
+                'class'                 => ['icinga-module', 'module-' . $moduleName],
+                'data-icinga-module'    => $moduleName
+            ]);
+        } elseif (method_exists($this, 'getDefaultTabControls')) {
+            foreach ($this->getDefaultTabControls() as $control) {
+                $this->addControl($control);
+            }
+        }
+
+        foreach ($hook->getContent($this->objectToLoadTabsFor) as $content) {
+            $this->addContent($content);
+        }
+
+        $this->content->addAttributes([
+            'class'                 => ['icinga-module', 'module-' . $moduleName],
+            'data-icinga-module'    => $moduleName
+        ]);
+
+        foreach ($hook->getFooter($this->objectToLoadTabsFor) as $footer) {
+            $this->addFooter($footer);
+        }
+
+        $this->footer->addAttributes([
+            'class'                 => ['icinga-module', 'module-' . $moduleName],
+            'data-icinga-module'    => $moduleName
+        ]);
+    }
+}
