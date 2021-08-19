f4b489b7c95084a725827449e27f248f46b30f68
Johannes Meyer <johannes.meyer@icinga.com>
2021-08-19 16:53:03 +0200

    Introduce widget `PluginOutputContainer`

---
+++ library/Icingadb/Widget/PluginOutputContainer.php
@@ -0,0 +1,22 @@
+<?php
+
+/* Icinga DB Web | (c) 2021 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Icingadb\Widget;
+
+use Icinga\Module\Icingadb\Util\PluginOutput;
+use ipl\Html\BaseHtmlElement;
+
+class PluginOutputContainer extends BaseHtmlElement
+{
+    protected $tag = 'div';
+
+    public function __construct(PluginOutput $output)
+    {
+        $this->setHtmlContent($output);
+
+        $this->getAttributes()->registerAttributeCallback('class', function () use ($output) {
+            return $output->isHtml() ? 'plugin-output' : 'plugin-output preformatted';
+        });
+    }
+}
