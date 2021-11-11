59e3d79c36caf97bf28a7bb5ac5e4eb6c07feb39
Johannes Meyer <johannes.meyer@icinga.com>
2021-11-11 16:16:37 +0100

    configuration.php: Don't load `migrate.js` for IE users

--- configuration.php
+++ configuration.php
@@ -570,6 +570,10 @@ namespace Icinga\Module\Icingadb {
     }
 
     $this->provideJsFile('action-list.js');
-    $this->provideJsFile('migrate.js');
     $this->provideJsFile('loadmore.js');
+
+    $forIe11 = (bool) preg_match('/Trident\/7.0;.*rv:11/', $_SERVER['HTTP_USER_AGENT']);
+    if (! $forIe11) {
+        $this->provideJsFile('migrate.js');
+    }
 }
