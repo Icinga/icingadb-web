8bcb5dc07b4f3b6ed31bf0e9459993a808abf1d1
Johannes Meyer <johannes.meyer@icinga.com>
2022-02-28 09:54:55 +0100

    PerfData: Render unkown UOMs as-is

--- library/Icingadb/Util/PerfData.php
+++ library/Icingadb/Util/PerfData.php
@@ -426,8 +426,8 @@ class PerfData
         $parts = explode(';', $this->perfdataValue);
 
         $matches = array();
-        if (preg_match('@^(-?\d+(\.\d+)?)([a-zA-Z%°]{1,3})$@u', $parts[0], $matches)) {
-            $this->unit = $matches[3];
+        if (preg_match('@^(-?(?:\d+)?(?:\.\d+)?)([a-zA-Z%°]{1,3})$@u', $parts[0], $matches)) {
+            $this->unit = $matches[2];
             $this->value = $matches[1];
         } else {
             $this->value = $parts[0];
@@ -551,7 +551,7 @@ class PerfData
             case ! is_numeric($value):
                 return $value;
             default:
-                return number_format($value, 2);
+                return number_format($value, 2) . ($this->unit !== null ? ' ' . $this->unit : '');
         }
     }
 
