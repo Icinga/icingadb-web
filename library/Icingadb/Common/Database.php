d54b731b4154af9cd85930de7f838a4126f7566c
Eric Lippmann <eric.lippmann@icinga.com>
2022-03-25 14:13:35 +0100

    PostgreSQL: Quote user
    user is a reserved key word in PostgreSQL, so we need to quote it.

--- library/Icingadb/Common/Database.php
+++ library/Icingadb/Common/Database.php
@@ -7,8 +7,10 @@ namespace Icinga\Module\Icingadb\Common;
 use Icinga\Application\Config as AppConfig;
 use Icinga\Data\ResourceFactory;
 use Icinga\Exception\ConfigurationError;
+use ipl\Sql\Adapter\Pgsql;
 use ipl\Sql\Config as SqlConfig;
 use ipl\Sql\Connection;
+use ipl\Sql\QueryBuilder;
 use PDO;
 
 trait Database
@@ -37,6 +39,18 @@ trait Database
             ];
 
             $this->db = new Connection($config);
+
+            $adapter = $this->db->getAdapter();
+            if ($adapter instanceof Pgsql) {
+                // user is a reserved key word in PostgreSQL, so we need to quote it.
+                // TODO(lippserd): This is pretty hacky, reconsider how to properly implement identifier quoting.
+                $quoted = $adapter->quoteIdentifier('user');
+                $this->db->getQueryBuilder()->on(QueryBuilder::ON_SELECT_ASSEMBLED, function (&$sql) use ($quoted) {
+                    $sql = str_replace(' user ', sprintf(' %s ', $quoted), $sql);
+                    $sql = str_replace(' user.', sprintf(' %s.', $quoted), $sql);
+                    $sql = str_replace('(user.', sprintf('(%s.', $quoted), $sql);
+                });
+            }
         }
 
         return $this->db;
