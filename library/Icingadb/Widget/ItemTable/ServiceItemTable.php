a336428f670e2afebfd2ba6eaee7283cc9eb9255
Johannes Meyer <johannes.meyer@icinga.com>
2022-05-23 13:38:51 +0200

    Introduce new widget class `ServiceItemTable`

---
+++ library/Icingadb/Widget/ItemTable/ServiceItemTable.php
@@ -0,0 +1,31 @@
+<?php
+
+/* Icinga DB Web | (c) 2022 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Icingadb\Widget\ItemTable;
+
+use Icinga\Module\Icingadb\Common\DetailActions;
+use Icinga\Module\Icingadb\Common\Links;
+use ipl\Web\Url;
+
+class ServiceItemTable extends StateItemTable
+{
+    use DetailActions;
+
+    protected function init()
+    {
+        $this->initializeDetailActions();
+        $this->setMultiselectUrl(Links::servicesDetails());
+        $this->setDetailUrl(Url::fromPath('icingadb/service'));
+    }
+
+    protected function getItemClass(): string
+    {
+        return ServiceRowItem::class;
+    }
+
+    protected function getVisualColumn(): string
+    {
+        return 'service.state.severity';
+    }
+}
