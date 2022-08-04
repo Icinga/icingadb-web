1734e35887a1e4346903b194f337be282a648c0b
Johannes Meyer <johannes.meyer@icinga.com>
2022-08-04 16:07:28 +0200

    Drop `view` param from show more urls in all index routes
    refs #463

--- application/controllers/DowntimesController.php
+++ application/controllers/DowntimesController.php
@@ -94,7 +94,7 @@ class DowntimesController extends Controller
 
         if ($compact) {
             $this->addContent(
-                (new ShowMore($results, Url::fromRequest()->without(['showCompact', 'limit'])))
+                (new ShowMore($results, Url::fromRequest()->without(['showCompact', 'limit', 'view'])))
                     ->setBaseTarget('_next')
                     ->setAttribute('title', sprintf(
                         t('Show all %d downtimes'),
--- application/controllers/ServicesController.php
+++ application/controllers/ServicesController.php
@@ -123,7 +123,7 @@ class ServicesController extends Controller
 
         if ($compact) {
             $this->addContent(
-                (new ShowMore($results, Url::fromRequest()->without(['showCompact', 'limit'])))
+                (new ShowMore($results, Url::fromRequest()->without(['showCompact', 'limit', 'view'])))
                     ->setBaseTarget('_next')
                     ->setAttribute('title', sprintf(
                         t('Show all %d services'),
