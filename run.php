2472b71722990f59f772b634e49c2d8f455f4137
Timm Ortloff <timm.ortloff@icinga.com>
2022-11-08 08:40:53 +0100

    Register EmailProvider

--- run.php
+++ run.php
@@ -10,6 +10,7 @@ $this->provideHook('health', 'IcingaHealth');
 $this->provideHook('health', 'RedisHealth');
 $this->provideHook('Reporting/Report', 'Reporting/HostSlaReport');
 $this->provideHook('Reporting/Report', 'Reporting/ServiceSlaReport');
+$this->provideHook('Reporting/EmailProvider', 'Reporting/EmailProvider');
 
 if (! $this::exists('monitoring')) {
     $modulePath = null;
