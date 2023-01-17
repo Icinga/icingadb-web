8a57ca7850ec644bbdd5bcd7552d6101965c072b
Johannes Meyer <johannes.meyer@icinga.com>
2023-01-17 17:44:08 +0100

    Introduce monitoring's perfdata tests here

---
+++ test/php/Lib/PerfdataSetWithPublicData.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Icinga DB Web | (c) 2023 Icinga GmbH | GPLv2 */
+
+namespace Tests\Icinga\Module\Icingadb\Lib;
+
+use Icinga\Module\Icingadb\Util\PerfDataSet;
+
+class PerfdataSetWithPublicData extends PerfdataSet
+{
+    public $perfdata = [];
+}
