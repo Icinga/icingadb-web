8e512152c9a312b48e5847d9aaf810c5236ba3e4
Johannes Meyer <johannes.meyer@icinga.com>
2023-05-26 14:28:00 +0200

    CustomvarFlat: Fix that variable names with dots are interpreted as paths
    fixes #757

--- library/Icingadb/Model/CustomvarFlat.php
+++ library/Icingadb/Model/CustomvarFlat.php
@@ -7,6 +7,7 @@ namespace Icinga\Module\Icingadb\Model;
 use ipl\Orm\Behavior\Binary;
 use ipl\Orm\Behaviors;
 use ipl\Orm\Model;
+use ipl\Orm\Query;
 use ipl\Orm\Relations;
 use Traversable;
 
@@ -92,12 +93,17 @@ class CustomvarFlat extends Model
      */
     public function unFlattenVars(Traversable $flattenedVars): array
     {
-        $registerValue = function (&$data, $path, $value) use (&$registerValue) {
+        $registerValue = function (&$data, $source, $path, $value) use (&$registerValue) {
             $step = array_shift($path);
-            $pos = null;
-            if (preg_match('/\[(\d+)]$/', $step, $m)) {
-                $step = substr($step, 0, -strlen($m[0]));
-                array_unshift($path, $m[1]);
+
+            $isIndex = (bool) preg_match('/^\[(\d+)]$/', $step, $m);
+            if ($isIndex) {
+                $step = $m[1];
+            }
+
+            while ($source !== null && ! empty($path) && (! is_array($source) || ! isset($source[$step]))) {
+                $step = sprintf($isIndex ? '[%d].%s' : '%s.%s', $step, array_shift($path));
+                $isIndex = false;
             }
 
             if (! empty($path)) {
@@ -105,16 +111,37 @@ class CustomvarFlat extends Model
                     $data[$step] = [];
                 }
 
-                $registerValue($data[$step], $path, $value);
+                $registerValue($data[$step], $source[$step] ?? null, $path, $value);
             } else {
                 $data[$step] = $value;
             }
         };
 
+        if ($flattenedVars instanceof Query) {
+            $flattenedVars->withColumns(['customvar.name', 'customvar.value']);
+        }
+
         $vars = [];
         foreach ($flattenedVars as $var) {
-            $path = explode('.', $var->flatname);
-            $registerValue($vars, $path, $var->flatvalue);
+            if (isset($var->customvar->name)) {
+                $var->customvar->value = json_decode($var->customvar->value, true);
+
+                $realName = $var->customvar->name;
+                $source = [$realName => $var->customvar->value];
+
+                $sourcePath = ltrim(substr($var->flatname, strlen($realName)), '.');
+                $path = array_merge([$realName], $sourcePath ? explode('.', $sourcePath) : []);
+            } else {
+                $path = explode('.', $var->flatname);
+                $source = null;
+            }
+
+            $registerValue($vars, $source, $path, $var->flatvalue);
+
+            if (isset($var->customvar->name)) {
+                $var->customvar->name = null;
+                $var->customvar->value = null;
+            }
         }
 
         return $vars;
