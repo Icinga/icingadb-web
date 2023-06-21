515ae13e326ef2c484cb2bd95d9be5adc1351d62
Johannes Meyer <johannes.meyer@icinga.com>
2023-06-21 15:24:52 +0200

    CheckStatistics: Make progress animatable and enhance overall layout

--- library/Icingadb/Widget/Detail/CheckStatistics.php
+++ library/Icingadb/Widget/Detail/CheckStatistics.php
@@ -8,10 +8,13 @@ use Icinga\Date\DateFormatter;
 use Icinga\Module\Icingadb\Widget\CheckAttempt;
 use Icinga\Module\Icingadb\Widget\EmptyState;
 use Icinga\Util\Format;
+use ipl\Html\Attributes;
 use ipl\Html\BaseHtmlElement;
-use ipl\Html\Html;
+use ipl\Html\FormattedString;
+use ipl\Html\HtmlElement;
+use ipl\Html\HtmlString;
+use ipl\Html\Text;
 use ipl\Web\Common\Card;
-use ipl\Web\Widget\HorizontalKeyValue;
 use ipl\Web\Widget\StateBall;
 use ipl\Web\Widget\TimeAgo;
 use ipl\Web\Widget\TimeSince;
@@ -20,11 +23,28 @@ use ipl\Web\Widget\VerticalKeyValue;
 
 class CheckStatistics extends Card
 {
+    const TOP_LEFT_BUBBLE_FLAG = <<<'SVG'
+<svg viewBox='0 0 12 12' xmlns='http://www.w3.org/2000/svg'>
+    <path class='bg' d='M0 0L13 13L3.15334e-06 13L0 0Z'/>
+    <path class='border' fill-rule='evenodd' clip-rule='evenodd'
+          d='M0 0L3.3959e-06 14L14 14L0 0ZM1 2.41421L1 13L11.5858 13L1 2.41421Z'/>
+</svg>
+SVG;
+
+    const TOP_RIGHT_BUBBLE_FLAG = <<<'SVG'
+<svg viewBox='0 0 12 12' xmlns='http://www.w3.org/2000/svg'>
+    <path class='bg' d="M12 0L-1 13L12 13L12 0Z"/>
+    <path class='border' fill-rule="evenodd" clip-rule="evenodd"
+          d="M12 0L12 14L-2 14L12 0ZM11 2.41421L11 13L0.414213 13L11 2.41421Z"/>
+</svg>
+SVG;
+
+
     protected $object;
 
     protected $tag = 'div';
 
-    protected $defaultAttributes = ['class' => 'progress-bar check-statistics'];
+    protected $defaultAttributes = ['class' => ['progress-bar', 'check-statistics']];
 
     public function __construct($object)
     {
@@ -35,96 +55,155 @@ class CheckStatistics extends Card
     {
         $hPadding = 10;
         $durationScale = 80;
+        $checkInterval = $this->getCheckInterval();
 
-        $timeline = Html::tag('div', ['class' => 'check-timeline timeline']);
-
+        $timeline = new HtmlElement('div', Attributes::create(['class' => ['check-timeline', 'timeline']]));
+        $above = new HtmlElement('ul', Attributes::create(['class' => 'above']));
+        $below = new HtmlElement('ul', Attributes::create(['class' => 'below']));
+        $progressBar = new HtmlElement('div', Attributes::create(['class' => 'bar']));
         $overdueBar = null;
 
-        $nextCheckTime = $this->object->state->next_check->getTimestamp();
-        $checkInterval = $this->getCheckInterval();
+        $now = time();
+        $executionTime = ($this->object->state->execution_time / 1000) + ($this->object->state->latency / 1000);
+
+        $nextCheckTime = $this->object->state->next_check !== null
+            ? $this->object->state->next_check->getTimestamp()
+            : null;
         if ($this->object->state->is_overdue) {
             $nextCheckTime = $this->object->state->next_update->getTimestamp();
-            $leftNow = $durationScale + $hPadding / 2;
 
-            $overdueScale = ($durationScale / 2) * (time() - $nextCheckTime) / (10 * $checkInterval);
-            if ($overdueScale > $durationScale / 2) {
-                $overdueScale = $durationScale / 2;
-            }
+            $durationScale = 60;
 
-            $durationScale -= $overdueScale;
-            $overdueBar = Html::tag('div', [
-                'class' => 'timeline-overlay check-overdue',
-                'style' => sprintf(
-                    'left: %F%%; width: %F%%;',
-                    $hPadding + $durationScale,
-                    $overdueScale + $hPadding / 2
+            $overdueBar = new HtmlElement(
+                'div',
+                Attributes::create(['class' => 'timeline-overlay']),
+                new HtmlElement('div', Attributes::create(['class' => 'now']))
+            );
+
+            $above->addHtml(new HtmlElement(
+                'li',
+                Attributes::create(['class' => 'now']),
+                new HtmlElement(
+                    'div',
+                    Attributes::create(['class' => 'bubble']),
+                    new HtmlElement('strong', null, Text::create(t('Now')))
                 )
-            ]);
+            ));
+
+            $this->getAttributes()->add('class', 'check-overdue');
         } else {
-            $leftNow = $durationScale * (1 - ($nextCheckTime - time()) / $checkInterval);
-            if ($leftNow > $durationScale) {
-                $leftNow = $durationScale;
+            $progressBar->addHtml(new HtmlElement('div', Attributes::create(['class' => 'now'])));
+        }
+
+        if ($nextCheckTime !== null && $nextCheckTime < $now) {
+            $lastUpdateTime = $nextCheckTime;
+            $nextCheckTime = $this->object->state->next_update->getTimestamp() - $executionTime;
+            $executionEndTime = $lastUpdateTime + $executionTime;
+        } else {
+            $lastUpdateTime = $this->object->state->last_update !== null
+                ? $this->object->state->last_update->getTimestamp() - $executionTime
+                : null;
+            $executionEndTime = $this->object->state->last_update !== null
+                ? $this->object->state->last_update->getTimestamp()
+                : null;
+        }
+
+        if ($this->object->state->is_overdue) {
+            $leftNow = 100;
+        } elseif ($nextCheckTime === null) {
+            $leftNow = 0;
+        } else {
+            $leftNow = 100 * (1 - ($nextCheckTime - time()) / ($nextCheckTime - $lastUpdateTime));
+            if ($leftNow > 100) {
+                $leftNow = 100;
             } elseif ($leftNow < 0) {
                 $leftNow = 0;
             }
         }
 
-        $above = Html::tag('ul', ['class' => 'above']);
-        $now = Html::tag(
-            'li',
-            [
-                'class' => 'now positioned',
-                'style' => sprintf('left: %F%%', $hPadding + $leftNow)
-            ],
-            Html::tag(
-                'div',
-                ['class' => 'bubble'],
-                Html::tag(
-                    'strong',
-                    t('Now')
-                )
-            )
-        );
-        $above->add($now);
+        $progressBar->getAttributes()->add('style', sprintf('width: %s%%', $leftNow));
 
-        $markerLast = Html::tag('div', [
-            'class' => 'marker start',
-            'style' => 'left: ' . $hPadding . '%',
-            'title' => $this->object->state->last_update !== null
-                ? DateFormatter::formatDateTime($this->object->state->last_update->getTimestamp())
-                : null
-        ]);
-        $markerNext = Html::tag('div', [
-            'class' => 'marker end',
-            'style' => sprintf('left: %F%%', $hPadding + $durationScale),
+        $leftExecutionEnd = $nextCheckTime !== null ? $durationScale * (
+            1 - ($nextCheckTime - $executionEndTime) / ($nextCheckTime - $lastUpdateTime)
+        ) : 0;
+
+        $markerLast = new HtmlElement('div', Attributes::create([
+            'class' => ['highlighted', 'marker', 'left'],
+            'title' => $lastUpdateTime !== null ? DateFormatter::formatDateTime($lastUpdateTime) : null
+        ]));
+        $markerNext = new HtmlElement('div', Attributes::create([
+            'class' => ['highlighted', 'marker', 'right'],
             'title' => $nextCheckTime !== null ? DateFormatter::formatDateTime($nextCheckTime) : null
-        ]);
-        $markerNow = Html::tag('div', [
-            'class' => 'marker now',
-            'style' => sprintf('left: %F%%', $hPadding + $leftNow),
-        ]);
+        ]));
+        $markerExecutionEnd = new HtmlElement('div', Attributes::create([
+            'class' => ['highlighted', 'marker'],
+            'style' => sprintf('left: %F%%', $hPadding + $leftExecutionEnd),
+        ]));
 
-        $timeline->add([
+        $progress = new HtmlElement('div', Attributes::create([
+            'class' => ['progress', time() < $executionEndTime ? 'running' : null]
+        ]), $progressBar);
+        if ($nextCheckTime !== null) {
+            $progress->addAttributes([
+                'data-animate-progress' => true,
+                'data-start-time' => $lastUpdateTime,
+                'data-end-time' => $nextCheckTime,
+                'data-switch-after' => $executionTime,
+                'data-switch-class' => 'running'
+            ]);
+        }
+
+        $timeline->addHtml(
+            $progress,
             $markerLast,
-            $markerNow,
-            $markerNext,
-            $overdueBar
-        ]);
+            $markerExecutionEnd,
+            $markerNext
+        )->add($overdueBar);
 
-        $lastUpdate = Html::tag(
+        $executionStart = new HtmlElement(
             'li',
-            ['class' => 'start'],
-            Html::tag(
+            Attributes::create(['class' => 'left']),
+            new HtmlElement(
                 'div',
-                ['class' => 'bubble upwards'],
-                new VerticalKeyValue(t('Last update'), $this->object->state->last_update !== null
-                    ? new TimeAgo($this->object->state->last_update->getTimestamp())
-                    : t('PENDING'))
+                Attributes::create(['class' => ['bubble', 'upwards', 'top-right-aligned']]),
+                new VerticalKeyValue(
+                    t('Execution Start'),
+                    $lastUpdateTime ? new TimeAgo($lastUpdateTime) : t('PENDING')
+                ),
+                HtmlString::create(self::TOP_RIGHT_BUBBLE_FLAG)
             )
         );
-        $interval = Html::tag(
+        $executionEnd = new HtmlElement(
             'li',
-            ['class' => 'interval'],
+            Attributes::create([
+                'class' => 'positioned',
+                'style' => sprintf('left: %F%%', $hPadding + $leftExecutionEnd)
+            ]),
+            new HtmlElement(
+                'div',
+                Attributes::create(['class' => ['bubble', 'upwards', 'top-left-aligned']]),
+                new VerticalKeyValue(
+                    t('Execution End'),
+                    $executionEndTime !== null
+                        ? ($executionEndTime > $now
+                        ? new TimeUntil($executionEndTime)
+                        : new TimeAgo($executionEndTime))
+                        : t('PENDING')
+                ),
+                HtmlString::create(self::TOP_LEFT_BUBBLE_FLAG)
+            )
+        );
+
+        $intervalLine = new HtmlElement(
+            'li',
+            Attributes::create([
+                'class' => 'interval-line',
+                'style' => sprintf(
+                    'left: %F%%; width: %F%%;',
+                    $hPadding + $leftExecutionEnd,
+                    $durationScale - $leftExecutionEnd
+                )
+            ]),
             new VerticalKeyValue(
                 t('Interval'),
                 $checkInterval
@@ -132,6 +211,29 @@ class CheckStatistics extends Card
                     : (new EmptyState(t('n. a.')))->setTag('span')
             )
         );
+        $executionLine = new HtmlElement(
+            'li',
+            Attributes::create([
+                'class' => ['interval-line', 'execution-line'],
+                'style' => sprintf('left: %F%%; width: %F%%;', $hPadding, $leftExecutionEnd)
+            ]),
+            new VerticalKeyValue(
+                sprintf('%s / %s', t('Execution Time'), t('Latency')),
+                FormattedString::create(
+                    '%s / %s',
+                    $this->object->state->execution_time !== null
+                        ? Format::seconds($this->object->state->execution_time / 1000)
+                        : (new EmptyState(t('n. a.')))->setTag('span'),
+                    $this->object->state->latency !== null
+                        ? Format::seconds($this->object->state->latency / 1000)
+                        : (new EmptyState(t('n. a.')))->setTag('span')
+                )
+            )
+        );
+        if ($executionEndTime !== null) {
+            $executionLine->addHtml(new HtmlElement('div', Attributes::create(['class' => 'start'])));
+            $executionLine->addHtml(new HtmlElement('div', Attributes::create(['class' => 'end'])));
+        }
 
         if ($this->isChecksDisabled()) {
             $nextCheckBubbleContent = new VerticalKeyValue(
@@ -145,34 +247,34 @@ class CheckStatistics extends Card
                 ? new VerticalKeyValue(t('Overdue'), new TimeSince($nextCheckTime))
                 : new VerticalKeyValue(
                     t('Next Check'),
-                    $nextCheckTime !== null ? new TimeUntil($nextCheckTime) : t('PENDING')
+                    $nextCheckTime !== null
+                        ? ($nextCheckTime > $now
+                            ? new TimeUntil($nextCheckTime)
+                            : new TimeAgo($nextCheckTime))
+                        : t('PENDING')
                 );
         }
 
-        $nextCheck = Html::tag(
+        $nextCheck = new HtmlElement(
             'li',
-            ['class' => 'end'],
-            Html::tag(
+            Attributes::create(['class' => 'right']),
+            new HtmlElement(
                 'div',
-                ['class' => 'bubble upwards'],
+                Attributes::create(['class' => ['bubble', 'upwards']]),
                 $nextCheckBubbleContent
             )
         );
 
-        $below = Html::tag(
-            'ul',
-            [
-                'class' => 'below',
-                'style' => sprintf('width: %F%%;', $durationScale)
-            ]
-        );
-        $below->add([
-            $lastUpdate,
-            $interval,
-            $nextCheck
-        ]);
+        $above->addHtml($executionLine);
 
-        $body->add([$above, $timeline, $below]);
+        $below->addHtml(
+            $executionStart,
+            $executionEnd,
+            $intervalLine,
+            $nextCheck
+        );
+
+        $body->addHtml($above, $timeline, $below);
     }
 
     /**
@@ -183,23 +285,6 @@ class CheckStatistics extends Card
     protected function isChecksDisabled(): bool
     {
         return ! ($this->object->active_checks_enabled || $this->object->passive_checks_enabled);
-    }
-
-    protected function assembleFooter(BaseHtmlElement $footer)
-    {
-        $footer->add(new HorizontalKeyValue(
-            t('Scheduling Source') . ':',
-            $this->object->state->scheduling_source ?? (new EmptyState(t('n. a.')))->setTag('span')
-        ));
-
-        if ($this->object->timeperiod->id) {
-            $footer->add(new HorizontalKeyValue(
-                t('Timeperiod') . ':',
-                $this->object->timeperiod->display_name ?? $this->object->timeperiod->name
-            ));
-
-            $footer->addAttributes(['class' => 'space-between']);
-        }
     }
 
     protected function assembleHeader(BaseHtmlElement $header)
@@ -213,26 +298,28 @@ class CheckStatistics extends Card
             ];
         }
 
-        $header->add([
+        $header->addHtml(
             new VerticalKeyValue(t('Command'), $this->object->checkcommand_name),
+            new VerticalKeyValue(
+                t('Scheduling Source'),
+                $this->object->state->scheduling_source ?? (new EmptyState(t('n. a.')))->setTag('span')
+            )
+        );
+
+        if ($this->object->timeperiod->id) {
+            $header->addHtml(new VerticalKeyValue(
+                t('Timeperiod'),
+                $this->object->timeperiod->display_name ?? $this->object->timeperiod->name
+            ));
+        }
+
+        $header->addHtml(
             new VerticalKeyValue(
                 t('Attempts'),
                 new CheckAttempt((int) $this->object->state->check_attempt, (int) $this->object->max_check_attempts)
             ),
-            new VerticalKeyValue(t('Check Source'), $checkSource),
-            new VerticalKeyValue(
-                t('Execution time'),
-                $this->object->state->execution_time
-                    ? Format::seconds($this->object->state->execution_time / 1000)
-                    : (new EmptyState(t('n. a.')))->setTag('span')
-            ),
-            new VerticalKeyValue(
-                t('Latency'),
-                $this->object->state->latency
-                    ? Format::seconds($this->object->state->latency / 1000)
-                    : (new EmptyState(t('n. a.')))->setTag('span')
-            )
-        ]);
+            new VerticalKeyValue(t('Check Source'), $checkSource)
+        );
     }
 
     /**
@@ -268,13 +355,13 @@ class CheckStatistics extends Card
         parent::assemble();
 
         if ($this->isChecksDisabled()) {
-            $this->add(Html::tag(
+            $this->addHtml(new HtmlElement(
                 'div',
-                ['class' => 'checks-disabled-overlay'],
-                Html::tag(
+                Attributes::create(['class' => 'checks-disabled-overlay']),
+                new HtmlElement(
                     'strong',
-                    ['class' => 'notes'],
-                    t('active and passive checks are disabled')
+                    Attributes::create(['class' => 'notes']),
+                    Text::create(t('active and passive checks are disabled'))
                 )
             ));
         }
