8a17a4f06cd32f028409e29686a81b2b15a44761
Johannes Meyer <johannes.meyer@icinga.com>
2024-03-21 10:19:48 +0100

    Controller: Make exported SQL queries copyable

--- library/Icingadb/Web/Controller.php
+++ library/Icingadb/Web/Controller.php
@@ -44,6 +44,7 @@ use ipl\Web\Control\LimitControl;
 use ipl\Web\Control\PaginationControl;
 use ipl\Web\Filter\QueryString;
 use ipl\Web\Url;
+use ipl\Web\Widget\CopyToClipboard;
 
 class Controller extends CompatController
 {
@@ -357,7 +358,9 @@ class Controller extends CompatController
                     $sql .= ' /* Unused values: "' . join('", "', $unused) . '" */';
                 }
 
-                $this->content->add(Html::tag('pre', $sql));
+                $pre = Html::tag('pre', $sql);
+                CopyToClipboard::attachTo($pre);
+                $this->content->add($pre);
             }
 
             return true;
