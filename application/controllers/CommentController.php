eebe825421849e764f3fc451c64798e3af01c7ca
Johannes Meyer <johannes.meyer@icinga.com>
2024-09-18 09:31:59 +0200

    controllers: Remove incorrect usage of trait `CommandActions`

--- application/controllers/CommentController.php
+++ application/controllers/CommentController.php
@@ -5,19 +5,14 @@
 namespace Icinga\Module\Icingadb\Controllers;
 
 use Icinga\Exception\NotFoundError;
-use Icinga\Module\Icingadb\Common\CommandActions;
-use Icinga\Module\Icingadb\Common\Links;
 use Icinga\Module\Icingadb\Model\Comment;
 use Icinga\Module\Icingadb\Web\Controller;
 use Icinga\Module\Icingadb\Widget\Detail\CommentDetail;
 use Icinga\Module\Icingadb\Widget\Detail\ObjectHeader;
 use ipl\Stdlib\Filter;
-use ipl\Web\Url;
 
 class CommentController extends Controller
 {
-    use CommandActions;
-
     /** @var Comment The comment object */
     protected $comment;
 
@@ -54,15 +49,5 @@ class CommentController extends Controller
         $this->addContent(new CommentDetail($this->comment));
 
         $this->setAutorefreshInterval(10);
-    }
-
-    protected function fetchCommandTargets(): array
-    {
-        return [$this->comment];
-    }
-
-    protected function getCommandTargetsUrl(): Url
-    {
-        return Links::comment($this->comment);
     }
 }
