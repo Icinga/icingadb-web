3914d947de0e5c4510752a403e6cd3b4cb4f0e30
raviks789 <raviks789@gmail.com>
2024-09-26 12:11:36 +0200

    Add `HasRootProblem` behavior for service

--- application/controllers/ServiceController.php
+++ application/controllers/ServiceController.php
@@ -38,13 +38,15 @@ class ServiceController extends Controller
         $name = $this->params->getRequired('name');
         $hostName = $this->params->getRequired('host.name');
 
-        $query = Service::on($this->getDb())->with([
-            'state',
-            'icon_image',
-            'host',
-            'host.state',
-            'timeperiod'
-        ]);
+        $query = Service::on($this->getDb())
+            ->withColumns(['has_root_problem'])
+            ->with([
+                'state',
+                'icon_image',
+                'host',
+                'host.state',
+                'timeperiod'
+            ]);
         $query
             ->setResultSetClass(VolatileStateResults::class)
             ->filter(Filter::all(
