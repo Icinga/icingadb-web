4033b39e7e5d2db0d8f93f91ecf829abf40a6e34
raviks789 <raviks789@gmail.com>
2024-10-01 14:57:07 +0200

    RedundancyGroupParentStateSummary: Group by all non-aggregrate columns for postgres

--- library/Icingadb/Model/RedundancyGroupParentStateSummary.php
+++ library/Icingadb/Model/RedundancyGroupParentStateSummary.php
@@ -5,8 +5,10 @@
 namespace Icinga\Module\Icingadb\Model;
 
 use ipl\Orm\Query;
+use ipl\Sql\Adapter\Pgsql;
 use ipl\Sql\Connection;
 use ipl\Sql\Expression;
+use ipl\Sql\Select;
 
 /**
  * Redundancy group's parent nodes summary
@@ -31,7 +33,7 @@ class RedundancyGroupParentStateSummary extends RedundancyGroup
                 'SUM(CASE WHEN redundancy_group_from_to_host_state.soft_state = 1'
                 . ' AND (redundancy_group_from_to_host_state.is_handled = \'y\''
                 . ' OR redundancy_group_from_to_host_state.is_reachable = \'n\') THEN 1 ELSE 0 END'
-                . '+ CASE WHEN redundancy_group_from_to_service_state.soft_state = 2'
+                . ' + CASE WHEN redundancy_group_from_to_service_state.soft_state = 2'
                 . ' AND (redundancy_group_from_to_service_state.is_handled = \'y\''
                 . ' OR redundancy_group_from_to_service_state.is_reachable = \'n\') THEN 1 ELSE 0 END)'
             ),
@@ -45,12 +47,12 @@ class RedundancyGroupParentStateSummary extends RedundancyGroup
             ),
             'objects_pending' => new Expression(
                 'SUM(CASE WHEN redundancy_group_from_to_host_state.soft_state = 99 THEN 1 ELSE 0 END'
-                . '+ CASE WHEN redundancy_group_from_to_service_state.soft_state = 99 THEN 1 ELSE 0 END)'
+                . ' + CASE WHEN redundancy_group_from_to_service_state.soft_state = 99 THEN 1 ELSE 0 END)'
             ),
             'objects_problems_unacknowledged' => new Expression(
                 'SUM(CASE WHEN redundancy_group_from_to_host_state.is_problem = \'y\''
                 . ' AND redundancy_group_from_to_host_state.is_acknowledged = \'n\' THEN 1 ELSE 0 END'
-                . '+ CASE WHEN redundancy_group_from_to_service_state.is_problem = \'y\''
+                . ' + CASE WHEN redundancy_group_from_to_service_state.is_problem = \'y\''
                 . ' AND redundancy_group_from_to_service_state.is_acknowledged = \'n\' THEN 1 ELSE 0 END)'
             ),
             'objects_total' => new Expression(
@@ -59,7 +61,7 @@ class RedundancyGroupParentStateSummary extends RedundancyGroup
             ),
             'objects_ok' => new Expression(
                 'SUM(CASE WHEN redundancy_group_from_to_host_state.soft_state = 0 THEN 1 ELSE 0 END'
-                . '+ CASE WHEN redundancy_group_from_to_service_state.soft_state = 0 THEN 1 ELSE 0 END)'
+                . ' + CASE WHEN redundancy_group_from_to_service_state.soft_state = 0 THEN 1 ELSE 0 END)'
             ),
             'objects_unknown_handled' => new Expression(
                 'SUM(CASE WHEN redundancy_group_from_to_service_state.soft_state = 3'
@@ -97,6 +99,26 @@ class RedundancyGroupParentStateSummary extends RedundancyGroup
         /** @var static $m */
         $m = $q->getModel();
         $q->columns($m->getSummaryColumns());
+
+        $q->on($q::ON_SELECT_ASSEMBLED, function (Select $select) use ($q) {
+            $model = $q->getModel();
+
+            $groupBy = $q->getResolver()->qualifyColumnsAndAliases((array) $model->getKeyName(), $model, false);
+
+            // For PostgreSQL, ALL non-aggregate SELECT columns must appear in the GROUP BY clause:
+            if ($q->getDb()->getAdapter() instanceof Pgsql) {
+                /**
+                 * Ignore Expressions, i.e. aggregate functions {@see getColumns()},
+                 * which do not need to be added to the GROUP BY.
+                 */
+                $candidates = array_filter($select->getColumns(), 'is_string');
+                // Remove already considered columns for the GROUP BY, i.e. the primary key.
+                $candidates = array_diff_assoc($candidates, $groupBy);
+                $groupBy = array_merge($groupBy, $candidates);
+            }
+
+            $select->groupBy($groupBy);
+        });
 
         return $q;
     }
