59ca9c2974eaac7f052b489c929f0e37bb75b13c
raviks789 <raviks789@gmail.com>
2024-10-11 10:49:16 +0200

    fix: HasRootProblem behavior

--- library/Icingadb/Model/Behavior/HasRootProblem.php
+++ library/Icingadb/Model/Behavior/HasRootProblem.php
@@ -4,6 +4,7 @@
 
 namespace Icinga\Module\Icingadb\Model\Behavior;
 
+use Icinga\Module\Icingadb\Common\Database;
 use Icinga\Module\Icingadb\Model\DependencyEdge;
 use ipl\Orm\AliasedExpression;
 use ipl\Orm\ColumnDefinition;
@@ -19,6 +20,8 @@ use ipl\Orm\Contract\QueryAwareBehavior;
  */
 class HasRootProblem implements RewriteColumnBehavior, QueryAwareBehavior
 {
+    use Database;
+
     /** @var Query */
     protected $query;
 
@@ -35,19 +38,33 @@ class HasRootProblem implements RewriteColumnBehavior, QueryAwareBehavior
             return null;
         }
 
-        $path = 'from.dependency_node';
+        $path = 'from.';
         $subQueryRelation = $relation !== null ? $relation . $path : $path;
-        $subQuery = $this->query->createSubQuery(new DependencyEdge(), $subQueryRelation)
+        $subQuery = $this->query->createSubQuery(new DependencyEdge(), $subQueryRelation, null, false)
             ->limit(1)
             ->columns([new Expression('1')]);
 
-        $subQuery->getSelectBase()->join(
-            ['root_dependency' => 'dependency'],
-            [$subQuery->getResolver()->getAlias($subQuery->getModel()) . '.dependency_id = root_dependency.id']
-        )->join(
-            ['root_dependency_state' => 'dependency_state'],
-            ['root_dependency.id = root_dependency_state.dependency_id']
-        )->where(new Expression("root_dependency_state.failed = 'y'"));
+        $subQueryAlias = $subQuery->getResolver()->getAlias($subQuery->getModel());
+
+        $subQuery->getSelectBase()
+            ->join(
+                ['to_dependency_node' => 'dependency_node'],
+                ["to_dependency_node.id = $subQueryAlias.to_node_id"]
+            )->joinLeft(
+                ['root_dependency' => 'dependency'],
+                [ "$subQueryAlias.dependency_id = root_dependency.id"]
+            )->joinLeft(
+                ['root_dependency_state' => 'dependency_state'],
+                ['root_dependency.id = root_dependency_state.dependency_id']
+            )->joinLeft(
+                ['root_group' => 'redundancy_group'],
+                ['root_group.id = to_dependency_node.redundancy_group_id']
+            )->joinLeft(
+                ['root_group_state' => 'redundancy_group_state'],
+                ['root_group_state.redundancy_group_id = root_group.id']
+            )->where(
+                new Expression("root_dependency_state.failed = 'y' OR root_group_state.failed = 'y'")
+            )->where($subQueryAlias . '_from.service_id = service.id');
 
         $column = $relation !== null ? str_replace('.', '_', $relation) . "_$column" : $column;
 
