eeb56ff33c86fc277afde93b8da82f4287f4cd78
Johannes Meyer <johannes.meyer@icinga.com>
2024-12-17 10:40:35 +0100

    db: Add model for the schema table

---
+++ library/Icingadb/Model/Schema.php
@@ -0,0 +1,50 @@
+<?php
+
+/* Icinga DB Web | (c) 2024 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Icingadb\Model;
+
+use DateTime;
+use ipl\Orm\Behavior\Binary;
+use ipl\Orm\Behavior\MillisecondTimestamp;
+use ipl\Orm\Behaviors;
+use ipl\Orm\Model;
+
+/**
+ * Schema model
+ *
+ * @property string $id
+ * @property int $version
+ * @property DateTime $timestamp
+ */
+class Schema extends Model
+{
+    public function getTableName(): string
+    {
+        return 'icingadb_schema';
+    }
+
+    public function getKeyName(): string
+    {
+        return 'id';
+    }
+
+    public function getColumns(): array
+    {
+        return [
+            'version',
+            'timestamp'
+        ];
+    }
+
+    public function getDefaultSort(): array
+    {
+        return ['timestamp desc'];
+    }
+
+    public function createBehaviors(Behaviors $behaviors): void
+    {
+        $behaviors->add(new Binary(['id']));
+        $behaviors->add(new MillisecondTimestamp(['timestamp']));
+    }
+}
