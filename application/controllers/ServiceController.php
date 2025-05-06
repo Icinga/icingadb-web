ab310c3e230ceb24ff23fcd6fdaf984b856b8c17
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2025-05-06 15:42:50 +0200

    (Host|Service)Controller: Reset the query offset
    This change was missing in the https://github.com/Icinga/icingadb-web/pull/734

--- application/controllers/ServiceController.php
+++ application/controllers/ServiceController.php
@@ -303,6 +303,7 @@ class ServiceController extends Controller
         $page = $paginationControl->getCurrentPageNumber();
 
         if ($page > 1 && ! $compact) {
+            $history->resetOffset();
             $history->limit($page * $limitControl->getLimit());
         }
 
