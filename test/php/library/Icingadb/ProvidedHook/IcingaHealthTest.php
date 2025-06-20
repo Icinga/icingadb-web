88c82e9bda07b8eca4c912f3613d02b84720bf28
Eric Lippmann <eric.lippmann@icinga.com>
2025-06-20 10:02:20 +0200

    Test `IcingaHealth::normalizeVersion()`

---
+++ test/php/library/Icingadb/ProvidedHook/IcingaHealthTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/* Icinga DB Web | (c) 2025 Icinga GmbH | GPLv2 */
+
+namespace Tests\Icinga\Modules\Icingadb\ProvidedHook;
+
+use Icinga\Module\Icingadb\ProvidedHook\IcingaHealth;
+use PHPUnit\Framework\TestCase;
+
+class IcingaHealthTest extends TestCase
+{
+    public function testNormalizeVersion()
+    {
+        $this->assertEquals('1.4.0', IcingaHealth::normalizeVersion('1.4.0'));
+        $this->assertEquals('1.4.0', IcingaHealth::normalizeVersion('1.4.0-1d5a35da5'));
+        $this->assertEquals('1.4.0', IcingaHealth::normalizeVersion('v1.4.0'));
+        $this->assertEquals('1.4.0', IcingaHealth::normalizeVersion('v1.4.0-1d5a35da5'));
+    }
+}
