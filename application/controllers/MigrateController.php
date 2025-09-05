e3aed04d97a58727185732f0ed5250a94fb54c87
Johannes Meyer <johannes.meyer@icinga.com>
2025-09-05 16:24:50 +0200

    migrate: Drop monitoring url preview

--- application/controllers/MigrateController.php
+++ application/controllers/MigrateController.php
@@ -4,8 +4,6 @@
 
 namespace Icinga\Module\Icingadb\Controllers;
 
-use Exception;
-use Icinga\Exception\IcingaException;
 use Icinga\Module\Icingadb\Compat\UrlMigrator;
 use Icinga\Module\Icingadb\Web\Controller;
 use ipl\Web\Filter\QueryString;
@@ -13,54 +11,6 @@ use ipl\Web\Url;
 
 class MigrateController extends Controller
 {
-    public function monitoringUrlAction()
-    {
-        $this->assertHttpMethod('post');
-        if (! $this->getRequest()->isApiRequest()) {
-            $this->httpBadRequest('No API request');
-        }
-
-        if (
-            ! preg_match('/([^;]*);?/', $this->getRequest()->getHeader('Content-Type'), $matches)
-            || $matches[1] !== 'application/json'
-        ) {
-            $this->httpBadRequest('No JSON content');
-        }
-
-        $urls = $this->getRequest()->getPost();
-
-        $result = [];
-        $errors = [];
-        foreach ($urls as $urlString) {
-            $url = Url::fromPath($urlString);
-            if (UrlMigrator::isSupportedUrl($url)) {
-                try {
-                    $urlString = rawurldecode(UrlMigrator::transformUrl($url)->getAbsoluteUrl());
-                } catch (Exception $e) {
-                    $errors[$urlString] = [
-                        IcingaException::describe($e),
-                        IcingaException::getConfidentialTraceAsString($e)
-                    ];
-                    $urlString = false;
-                }
-            }
-
-            $result[] = $urlString;
-        }
-
-        $response = $this->getResponse()->json();
-        if (empty($errors)) {
-            $response->setSuccessData($result);
-        } else {
-            $response->setFailData([
-                'result' => $result,
-                'errors' => $errors
-            ]);
-        }
-
-        $response->sendResponse();
-    }
-
     public function searchUrlAction()
     {
         $this->assertHttpMethod('post');
