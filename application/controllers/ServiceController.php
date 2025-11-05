458e770e91b1a5bc2b7f88b829a0a3e7553099c4
Jan Schuppik <jan.schuppik@icinga.com>
2025-11-06 00:24:38 +0100

    Adjust: open hosts of services in next column

--- application/controllers/ServiceController.php
+++ application/controllers/ServiceController.php
@@ -79,7 +79,7 @@ class ServiceController extends Controller
         $this->service = $service;
         $this->loadTabsForObject($service);
 
-        $this->addControl(new ObjectHeader($service));
+        $this->addControl((new ObjectHeader($service))->setAttributes(['data-base-target' => '_next']));
 
         $this->setTitleTab($this->getRequest()->getActionName());
         $this->setTitle(
