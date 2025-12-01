d29d13c455bbd7cfa937493eec3611a7594682e1
Bastian Lederer <bastian.lederer@icinga.com>
2025-12-01 14:20:10 +0100

    Fix usage of `PDO::MYSQL_ATTR_INIT_COMMAND`
    The constant is deprecated since php-8.5 but the replacement
    `Pdo\Mysql::ATTR_INIT_COMMAND` is only available since php-8.4

--- library/Icingadb/Common/Backend.php
+++ library/Icingadb/Common/Backend.php
@@ -60,7 +60,16 @@ final class Backend
 
             $config->options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ];
             if ($config->db === 'mysql') {
-                $config->options[PDO::MYSQL_ATTR_INIT_COMMAND] = "SET SESSION SQL_MODE='STRICT_TRANS_TABLES"
+                // In PHP 8.5+, driver-specific constants of the PDO class are deprecated,
+                // but the replacements are only available since php 8.4
+                if (version_compare(PHP_VERSION, '8.4.0', '<')) {
+                    $mysqlAttrInitCommand = PDO::MYSQL_ATTR_INIT_COMMAND;
+                } else {
+                    $mysqlAttrInitCommand = Pdo\Mysql::ATTR_INIT_COMMAND;
+                }
+
+                $config->options[$mysqlAttrInitCommand]
+                    = "SET SESSION SQL_MODE='STRICT_TRANS_TABLES"
                     . ",NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'";
             }
 
