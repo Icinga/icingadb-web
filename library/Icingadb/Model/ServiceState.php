2d92104488e5dec48428b6164acac6c48c8667f2
Gianluca Piccolo <2795394+gianlucapiccolo@users.noreply.github.com>
2026-03-02 15:33:42 +0100

    Fix: typo in service state translations

--- library/Icingadb/Model/ServiceState.php
+++ library/Icingadb/Model/ServiceState.php
@@ -85,6 +85,6 @@ class ServiceState extends State
 
     public function getStateTextTranslated(): string
     {
-        return ServiceStates::traslated($this->soft_state);
+        return ServiceStates::translated($this->soft_state);
     }
 }
