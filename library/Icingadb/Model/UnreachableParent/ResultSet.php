5808d2900606ef1aed28591b98adcaac0205e5b1
Bastian Lederer <bastian.lederer@icinga.com>
2026-03-06 14:02:47 +0100

    Ensure compatibility with `ipl\orm\ResultSet`
    Add retrun types to subclasses of `ResultSet` to make them forward-compatible
    with the ipl-orm

--- library/Icingadb/Model/UnreachableParent/ResultSet.php
+++ library/Icingadb/Model/UnreachableParent/ResultSet.php
@@ -2,12 +2,13 @@
 
 namespace Icinga\Module\Icingadb\Model\UnreachableParent;
 
+use Generator;
 use Icinga\Module\Icingadb\Redis\VolatileStateResults;
 use Traversable;
 
 class ResultSet extends VolatileStateResults
 {
-    protected function yieldTraversable(Traversable $traversable)
+    protected function yieldTraversable(Traversable $traversable): Generator
     {
         $knownIds = [];
         foreach ($traversable as $value) {
