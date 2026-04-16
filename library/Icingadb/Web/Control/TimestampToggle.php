c81be939328c6a0ccef5ca7cc71e551a2c695901
Bastian Lederer <bastian.lederer@icinga.com>
2026-04-16 07:48:46 +0200

    Remove type from class constant
    It is not supported by php 8.2

--- library/Icingadb/Web/Control/TimestampToggle.php
+++ library/Icingadb/Web/Control/TimestampToggle.php
@@ -10,7 +10,7 @@ use ipl\Web\Compat\CompatForm;
 class TimestampToggle extends CompatForm
 {
     /** @var string Default timestamp mode param */
-    public const string DEFAULT_TIMESTAMP_MODE_PARAM = 'timestamps';
+    public const DEFAULT_TIMESTAMP_MODE_PARAM = 'timestamps';
     /** @var bool Whether relative or absolute timestamps are to be used */
     protected bool $useRelativeTimestamps;
 
